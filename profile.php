659a00ff344781f51097d30833b8dabf4502cc77
Marc <BluntToothbrush>
2015-06-14 20:28:51 +0100

    Image title for the bricks

--- profile.php
+++ profile.php
@@ -70,22 +70,22 @@
             <div class="profilePosts">
                 <ul>
                     <li>
-                        <a href="" title="The Old Room of Macabre Detritus"><img src="img/cat.jpg" width="500" height="390" focus-y="3" focus-x="3"></a>
+                        <a href="" title=""><img src="img/cat.jpg" width="500" height="390" focus-y="3" focus-x="3"></a>
                     </li>
                     <li>
-                        <a href="" title="The Old Room of Macabre Detritus"><img src="img/cat.jpg" width="250" height="340" focus-y="3" focus-x="3"></a>
+                        <a href="" title=""><img src="img/cat.jpg" width="250" height="340" focus-y="3" focus-x="3"></a>
                     </li>
                     <li>
-                        <a href="" title="The Old Room of Macabre Detritus"><img src="img/cat.jpg" width="310" height="170" focus-y="3" focus-x="3"></a>
+                        <a href="" title=""><img src="img/cat.jpg" width="310" height="170" focus-y="3" focus-x="3"></a>
                     </li>
                     <li>
-                        <a href="" title="The Old Room of Macabre Detritus"><img src="img/cat.jpg" width="500" height="390" focus-y="3" focus-x="3"></a>
+                        <a href="" title=""><img src="img/cat.jpg" width="500" height="390" focus-y="3" focus-x="3"></a>
                     </li>
                     <li>
-                        <a href="" title="The Old Room of Macabre Detritus"><img src="img/cat.jpg" width="500" height="390" focus-y="3" focus-x="3"></a>
+                        <a href="" title=""><img src="img/cat.jpg" width="500" height="390" focus-y="3" focus-x="3"></a>
                     </li>
                     <li>
-                        <a href="" title="The Old Room of Macabre Detritus"><img src="img/cat.jpg" width="500" height="390" focus-y="3" focus-x="3"></a>
+                        <a href="" title=""><img src="img/cat.jpg" width="500" height="390" focus-y="3" focus-x="3"></a>
                     </li>
                 </ul>
             </div>
