35b89b0c76abd64f57eab396b9ff3890622dced0
Jamie932 <Jamie932@gmail.com>
2015-06-14 18:23:24 +0100

    Removed old javascript

--- js/checkLogin.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    if ($.cookie('username')) {
-        $('.in').add();
-        $('.out').remove();
-    } else {
-        $('.in').remove();
-        $('.out').add();
-    }
-});
