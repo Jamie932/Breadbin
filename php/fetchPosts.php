e9214f7c41d7a0614f69a54fdd9ef13beaa64f7a
Jamie932 <Jamie932@gmail.com>
2015-06-14 13:52:28 +0100

    Added username to post retreival

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -9,11 +9,26 @@
 	} 
 	catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$query = "SELECT * FROM users WHERE id = :id"; 
+		$query_params = array(':id' => $row['userid']); 
+	
+        try{ 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+		$row = $stmt->fetch();
+		$username = 'Unknown';
+				
+		if($row){ 
+			$username = $row['username'];
+		}
+		
 		echo '<div id="contentPost">';
 		echo '<div class="contentPostImage"></div>';
             echo '<div class="contentPostInfo">';
                 echo '<div id="contentInfoText">';
-                    echo '<div class="left">UsernameGoesHere</div>';
+                    echo '<div class="left">' . $username . '</div>';
                     echo '<div class="right">' . $row['date'] . '</div>';
                 echo '</div>';
             echo '</div>';
