eb60f340dd48e10d89ed9852186635c8eec5844f
Jamie932 <Jamie932@gmail.com>
2015-06-16 00:13:28 +0100

    Testing a massive verifyEmail change

--- php/verifyEmail.php
+++ php/verifyEmail.php
@@ -1,46 +1,3 @@
-<?php
-    require("common.php");
-    $successful = false;
-
-    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
-        $query = "SELECT * FROM uniquelogs WHERE hash = :hash";
-        $query_params = array(':hash' => $_POST['hash']); 
-        try { 
-            $stmt = $db->prepare($query); 
-            $result = $stmt->execute($query_params); 
-        } 
-        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
-
-        $row = $stmt->fetch(); 
-
-        if ($row) {
-            $userid = $row['userid'];
-            
-            $query = "SELECT * FROM users WHERE id = :id";
-            $query_params = array(':id' => $userid); 
-            try { 
-                $stmt = $db->prepare($query); 
-                $result = $stmt->execute($query_params); 
-            } 
-            catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
-
-            $row = $stmt->fetch(); 
-
-            if ($row) {
-                $query = "UPDATE users SET active=1 WHERE id = :id";
-                $query_params = array(':id' => $userid); 
-                try { 
-                    $stmt = $db->prepare($query); 
-                    $result = $stmt->execute($query_params); 
-                } 
-                catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }  
-
-                $successful = true;
-            }
-        }
-    }
-?>
-
 <html>
 <head>
     <title>Bread Bin</title>
@@ -50,49 +7,90 @@
     <script src="/js/jquery.cookie.js"></script>
 </head>
 <body>
+    <?php
+        require("common.php");
+        $successful = false;
+
+        if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+            $query = "SELECT * FROM uniquelogs WHERE hash = :hash";
+            $query_params = array(':hash' => $_POST['hash']); 
+            try { 
+                $stmt = $db->prepare($query); 
+                $result = $stmt->execute($query_params); 
+            } 
+            catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+
+            $row = $stmt->fetch(); 
+
+            if ($row) {
+                $userid = $row['userid'];
+
+                $query = "SELECT * FROM users WHERE id = :id";
+                $query_params = array(':id' => $userid); 
+                try { 
+                    $stmt = $db->prepare($query); 
+                    $result = $stmt->execute($query_params); 
+                } 
+                catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+
+                $row = $stmt->fetch(); 
+
+                if ($row) {
+                    $query = "UPDATE users SET active=1 WHERE id = :id";
+                    $query_params = array(':id' => $userid); 
+                    try { 
+                        $stmt = $db->prepare($query); 
+                        $result = $stmt->execute($query_params); 
+                    } 
+                    catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }  ?>
+
+                  <div id="mid">
+                        <div class="verify" style="display:block;">
+                            <div id="header">
+                                <h2>Success!</h2>
+                            </div>
+
+                            <hr></hr>
+
+                            <div style="font-size:0.9em;">
+                                <p>You've successfully validated your email.</p> 
+                            </div>
+
+                            <div class="dockBottom">
+                                <ul>
+                                    <li>Terms of Service</li>
+                                    <li>Privacy Policy</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
     
-    <?php if($successful){ ?>
-        <div id="mid">
-            <div class="verify">
-                <div id="header">
-                    <h2>Success!</h2>
-                </div>
+                <?php } else {  ?> 
+    
+                    <div id="mid">
+                        <div class="verify" style="display:block;">
+                            <div id="header">
+                                <h2>Error!</h2>
+                            </div>
 
-                <hr></hr>
+                            <hr></hr>
 
-                <div style="font-size:0.9em;">
-                    <p>You've successfully validated your email.</p> 
-                </div>
+                            <div style="font-size:0.9em;">
+                                <p>We encountered an error :(</p> 
+                            </div>
 
-                <div class="dockBottom">
-                    <ul>
-                        <li>Terms of Service</li>
-                        <li>Privacy Policy</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    <?php } else { ?>
-        <div id="mid">
-            <div class="verify">
-                <div id="header">
-                    <h2>Error!</h2>
-                </div>
-
-                <hr></hr>
-
-                <div style="font-size:0.9em;">
-                    <p>We encountered an error :(</p> 
-                </div>
-
-                <div class="dockBottom">
-                    <ul>
-                        <li>Terms of Service</li>
-                        <li>Privacy Policy</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    <?php } ?>
+                            <div class="dockBottom">
+                                <ul>
+                                    <li>Terms of Service</li>
+                                    <li>Privacy Policy</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                <?php  
+                }
+            }
+        }
+    ?>
 </body>
 </html>
