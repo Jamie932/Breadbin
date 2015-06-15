e1a263d4457544822f7636380d5fa9ff01650567
Marc <BluntToothbrush>
2015-06-15 18:25:54 +0100

    Posts test

--- php/userPosts.php
+++ php/userPosts.php
@@ -1,9 +1,9 @@
 <?php
     require("common.php"); 
-	require("timeago.php");
 	
-	$query = "SELECT * FROM posts WHERE id = :id ORDER BY date DESC"; 
-    $query_params = array(':id' => $_SESSION['user']['id']); 
+    foreach ($posts as $row) {
+		$query = "SELECT * FROM users WHERE id = :id"; 
+		$query_params = array(':id' => $_SESSION['user']['id']); 
         
     try{ 
             $stmt = $db->prepare($query); 
@@ -13,27 +13,11 @@
 		$userrow = $stmt->fetch();
 		$username = 'Unknown';
 
-    if ($row['type'] == "image") {  
-    echo '<div id="contentPost">';
-            echo '<div class="contentPostImage"></div>';
-                echo '<div class="contentPostInfo">';
-                    echo '<div id="contentInfoText">';
-                        echo '<div class="left"><a href="profile.php?id=' . $row['userid'] . '">' . $username . '</a></div>';
-                        echo '<div class="right">' . timeAgoInWords($row['date']) . '</div>';
-                    echo '</div>';
-                echo '</div>';
-            echo '</div>';
-            echo '<div id="contentLike"><P>Toast</P><P>Burn</P><P class="report">Report</P></div><br>';
-            
-        } else if ($row['type'] == "text") {
+    if ($row['type'] == "text") {
             echo '<div id="contentPost">';
                 echo '<div class="contentPostText">' . $row['text'] . '</div>';
-                echo '<div id="contentInfoText">';
-                    echo '<div class="left"><a href="profile.php?id=' . $row['userid'] . '">' . $username . '</a></div>';
-                    echo '<div class="right">' . timeAgoInWords($row['date']) . '</div>';
                 echo '</div>';
             echo '</div>';
-            echo '<div id="contentLike"><P>Toast</P><P>Burn</P><P class="report">Report</P></div><br>';
         }
     }
 ?>
