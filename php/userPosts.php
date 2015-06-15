bfad6327872ffb7162b51de6f163ff69263ba78e
Marc <BluntToothbrush>
2015-06-15 19:42:58 +0100

    User Post test again

--- php/userPosts.php
+++ php/userPosts.php
@@ -1,8 +1,9 @@
-<?php
-    require("php/common.php"); 
+ <?php 
+                require("php/common.php"); 
                 $userID = intval($_GET['id']);
+
                 $query = "SELECT * FROM users WHERE id = :id"; 
-                $query_params = array(':id' => $userID); 
+		        $query_params = array(':id' => $row['userid']); 
 
                 try{ 
                     $stmt = $db->prepare($query); 
@@ -11,11 +12,9 @@
                 catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }
                 $row = $stmt->fetch();
 
-    if ($row['type'] == "text") {
-            echo '<div id="contentPost">';
-                echo '<div class="contentPostText">' . $row['text'] . '</div>';
-                echo '</div>';
-            echo '</div>';
-        }
-    }
-?>
+                if($row){ 
+                    echo '<div class="contentPostText">' . $row['text'] . '</div>';
+                } else {
+                    echo "<div id=\"errormsg\"> User not found </div>";
+                }
+    ?>
