c225ae2323d3af6f526fd1e455c17c85e2f379a3
Jamie932 <Jamie932@gmail.com>
2015-06-15 14:18:25 +0100

    Navbar js

--- php/template/navbar.php
+++ php/template/navbar.php
@@ -1,3 +1,18 @@
+<script>
+	$(document).ready(function(){
+		$(window).scroll(function(){
+			var y = $(window).scrollTop();
+			if( y > 0 ){
+				$("#navbar").css({'box-shadow':'none', '-moz-box-shadow':'none', '-webkit-box-shadow':'none'});
+			} else {
+				$("#navbar").css({'box-shadow':'inset 0 1px #fff, 0 1px 3px rgba(34,25,25,0.4);', 
+					'-moz-box-shadow':'inset 0 1px #fff, 0 1px 3px rgba(34,25,25,0.4);', 
+					'-webkit-box-shadow':'inset 0 1px #fff, 0 1px 3px rgba(34,25,25,0.4);'});
+			}
+		});
+	})
+</script>
+
 <div id="navbar">
 	<div class="left">
 		<a href="main.php" class="navLinks">Bread Bin</a>
@@ -6,9 +21,9 @@
 	<div class="right">
 		<ul class="nav">		
 			<li class="nav">
-				<a class="navLinks" href="profile.php" >Profile</a>
+				<a class="navLinks" href="settings.php" >Profile</a>
 			</li>
 		</ul>
-	</div>
+	</div>
 	
 </div>
