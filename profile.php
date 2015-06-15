82285f84488352c125ee99f55b44f65af98b05d7
Marc <BluntToothbrush>
2015-06-15 20:05:19 +0100

    Profile page
    Try and try again

--- profile.php
+++ profile.php
@@ -58,7 +58,28 @@
 
     <div id="rightProfile">
         <div class="profilePosts">
-            <script src="php/userPosts.php"></script>
+            <?php 
+                require("common.php"); 
+
+                $userID = intval($_GET['id']);
+                $query = "SELECT * FROM users WHERE id = :id"; 
+                $query_params = array(':id' => $userID); 
+
+                try{ 
+                    $stmt = $db->prepare($query); 
+                    $result = $stmt->execute($query_params); 
+                } 
+                catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }
+                $row = $stmt->fetch();
+
+                if($row){ 
+                    echo 'User ID: ' . $row['id'] . '<br>';
+                    echo 'Username: ' . $row['username'] . '<br>';
+                    echo 'Email: ' . $row['email'];
+                } else {
+                    echo "<div id=\"errormsg\"> User not found </div>";
+                }
+?>
         </div>
     </div>
 </body>
