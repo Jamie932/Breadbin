2412247598e49ad6a6c4d1c7597c63151becaa6f
Jamie932 <Jamie932@gmail.com>
2015-06-16 00:17:49 +0100

    Completely changed verifyEmail again

--- php/verifyEmail.php
+++ php/verifyEmail.php
@@ -7,90 +7,68 @@
     <script src="/js/jquery.cookie.js"></script>
 </head>
 <body>
-    <?php
-        require("common.php");
-        $successful = false;
+    <div id="mid">
+        <div class="verify" style="display:block;">
+            <div id="header">
+                <h2>Email Validation!</h2>
+            </div>
 
-        if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
-            $query = "SELECT * FROM uniquelogs WHERE hash = :hash";
-            $query_params = array(':hash' => $_POST['hash']); 
-            try { 
-                $stmt = $db->prepare($query); 
-                $result = $stmt->execute($query_params); 
-            } 
-            catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+            <hr></hr>
 
-            $row = $stmt->fetch(); 
+            <div style="font-size:0.9em;">
+                <?php
+                    require("common.php");
+                    $successful = false;
 
-            if ($row) {
-                $userid = $row['userid'];
+                    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+                        $query = "SELECT * FROM uniquelogs WHERE hash = :hash";
+                        $query_params = array(':hash' => $_POST['hash']); 
+                        try { 
+                            $stmt = $db->prepare($query); 
+                            $result = $stmt->execute($query_params); 
+                        } 
+                        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
 
-                $query = "SELECT * FROM users WHERE id = :id";
-                $query_params = array(':id' => $userid); 
-                try { 
-                    $stmt = $db->prepare($query); 
-                    $result = $stmt->execute($query_params); 
-                } 
-                catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+                        $row = $stmt->fetch(); 
 
-                $row = $stmt->fetch(); 
+                        if ($row) {
+                            $userid = $row['userid'];
 
-                if ($row) {
-                    $query = "UPDATE users SET active=1 WHERE id = :id";
-                    $query_params = array(':id' => $userid); 
-                    try { 
-                        $stmt = $db->prepare($query); 
-                        $result = $stmt->execute($query_params); 
-                    } 
-                    catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }  ?>
+                            $query = "SELECT * FROM users WHERE id = :id";
+                            $query_params = array(':id' => $userid); 
+                            try { 
+                                $stmt = $db->prepare($query); 
+                                $result = $stmt->execute($query_params); 
+                            } 
+                            catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
 
-                  <div id="mid">
-                        <div class="verify" style="display:block;">
-                            <div id="header">
-                                <h2>Success!</h2>
-                            </div>
+                            $row = $stmt->fetch(); 
 
-                            <hr></hr>
+                            if ($row) {
+                                $query = "UPDATE users SET active=1 WHERE id = :id";
+                                $query_params = array(':id' => $userid); 
+                                try { 
+                                    $stmt = $db->prepare($query); 
+                                    $result = $stmt->execute($query_params); 
+                                } 
+                                catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }  
 
-                            <div style="font-size:0.9em;">
-                                <p>You've successfully validated your email.</p> 
-                            </div>
+                                echo "<p>You've successfully validated your email.</p>";
+                            } else {
+                                echo "<p>We encountered an error :(</p>";
+                            }
+                        }
+                    }
+                ?>
+            </div>
 
-                            <div class="dockBottom">
-                                <ul>
-                                    <li>Terms of Service</li>
-                                    <li>Privacy Policy</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-    
-                <?php } else {  ?> 
-    
-                    <div id="mid">
-                        <div class="verify" style="display:block;">
-                            <div id="header">
-                                <h2>Error!</h2>
-                            </div>
-
-                            <hr></hr>
-
-                            <div style="font-size:0.9em;">
-                                <p>We encountered an error :(</p> 
-                            </div>
-
-                            <div class="dockBottom">
-                                <ul>
-                                    <li>Terms of Service</li>
-                                    <li>Privacy Policy</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                <?php  
-                }
-            }
-        }
-    ?>
+            <div class="dockBottom">
+                <ul>
+                    <li>Terms of Service</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
