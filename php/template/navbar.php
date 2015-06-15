800c4cbfa1ba7f85ddabc843ee6fbcde5d518d82
Jamie932 <Jamie932@gmail.com>
2015-06-15 20:05:13 +0100

    Added a logout to the navbar

--- php/template/navbar.php
+++ php/template/navbar.php
@@ -1,3 +1,22 @@
+<script>
+    function logout() {
+        if (isset($_COOKIE['hashkey'])) {
+            unset($_COOKIE['hashkey']);
+            
+            $query = "DELETE FROM uniquelogs WHERE hash = :hash"; 
+            $query_params = array(':hash' => $_COOKIE['hashkey']); 
+
+            try{ 
+                $stmt = $db->prepare($query); 
+                $result = $stmt->execute($query_params); 
+            } 
+            catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }   
+            
+            window.location.href = "index.php";
+        }
+    }
+</script>
+
 <div id="navbar">
 	<div class="left">
 		<a href="main.php" class="navLinks">Bread Bin</a>
@@ -10,6 +29,8 @@
 				    echo '<a class="navLinks" href="profile.php?id=' . $_SESSION['user']['id'] . '">Profile</a>';
                 ?>
 			</li>
+            
+            <li class="nav"><a onClick="logout(); return false;">Logout</a></li>
 		</ul>
 	</div>
 </div>
