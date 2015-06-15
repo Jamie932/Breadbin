6a141cf672229438b43e7c335b1a8730d8827212
Jamie932 <Jamie932@gmail.com>
2015-06-15 14:20:34 +0100

    Reverted navbar change

--- php/template/navbar.php
+++ php/template/navbar.php
@@ -1,18 +1,3 @@
-<script>
-	$(document).ready(function(){
-		$(window).scroll(function(){
-			var y = $(window).scrollTop();
-			if( y > 0 ){
-				$("#navbar").css({'box-shadow':'none', '-moz-box-shadow':'none', '-webkit-box-shadow':'none'});
-			} else {
-				$("#navbar").css({'box-shadow':'inset 0 1px #fff, 0 1px 3px rgba(34,25,25,0.4);', 
-					'-moz-box-shadow':'inset 0 1px #fff, 0 1px 3px rgba(34,25,25,0.4);', 
-					'-webkit-box-shadow':'inset 0 1px #fff, 0 1px 3px rgba(34,25,25,0.4);'});
-			}
-		});
-	})
-</script>
-
 <div id="navbar">
 	<div class="left">
 		<a href="main.php" class="navLinks">Bread Bin</a>
@@ -24,6 +9,5 @@
 				<a class="navLinks" href="settings.php" >Profile</a>
 			</li>
 		</ul>
-	</div>
-	
+	</div>
 </div>
