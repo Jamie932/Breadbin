a3b778619519ecd1e00440e2ffe8ff5dcf2c6fe0
Marc <BluntToothbrush>
2015-06-18 00:41:43 +0100

    pls work

--- php/unfollow.php
+++ php/unfollow.php
@@ -1,7 +1,7 @@
 <?php
     require("common.php");
-
-    $query = "DELETE FROM following WHERE :follower = :id"; 
+    
+    $query = "DELETE FROM following WHERE user_no = :id AND follow_id = :follower"; 
     $query_params = array(':id' => $_POST['url'], ':follower' =>  $_SESSION['user']['id']);
     $stmt = $db->prepare($query); 
     $result = $stmt->execute($query_params);                       
