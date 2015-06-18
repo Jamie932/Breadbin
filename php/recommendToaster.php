58407bfcc8d9b2429adbdc42301e75f09432a07a
Marc <BluntToothbrush>
2015-06-18 13:52:57 +0100

    and again ll

--- php/recommendToaster.php
+++ php/recommendToaster.php
@@ -4,12 +4,11 @@
     $query = "SELECT username FROM users ORDER BY RAND() LIMIT 1"; 
         $stmt = $db->prepare($query); 
         $result = $stmt->execute(); 
-        $username = $stmt->fetch();
 
         echo '<div class="userRecom">';
             echo '<div class="usericoRecom">';
                 echo '<img src="img/cat.jpg" height="50px" width="50px">';
             echo '</div>';
-            echo '<div class="usernameRecom">' .$username . '</div>';
+            echo '<div class="usernameRecom">' .$result . '</div>';
         echo '</div>';
 ?>
