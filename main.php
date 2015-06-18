622e58e02977365be367075d7805161e90804c77
Jamie932 <Jamie932@gmail.com>
2015-06-18 19:06:44 +0100

    Changed JQuery handler

--- main.php
+++ main.php
@@ -13,7 +13,7 @@
     <script src="js/jquery.cookie.js"></script>
     <script>
         $(document).ready(function(){
-            $(document).on('click','.delete', function() {             
+            $('.delete').click(            
                 var confirmed = confirm("Are you sure you want to delete this post?");
       
                 if (confirmed) {
