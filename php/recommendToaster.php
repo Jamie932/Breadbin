19be894513f0677fb47d156425480b2d24d58b91
Marc <BluntToothbrush>
2015-06-18 14:45:44 +0100

    maybe fixed

--- php/recommendToaster.php
+++ php/recommendToaster.php
@@ -1,14 +1,12 @@
 <?php 
     require("php/common.php");
 
-        $query = "SELECT * FROM users WHERE id NOT IN (SELECT user_no FROM following WHERE follower_id=users.id AND user_id=:id)) ORDER BY RAND() LIMIT 3"; 
+        $query = "SELECT * FROM users WHERE id NOT IN (SELECT user_no FROM following WHERE follower_id=users.id AND user_id=:id) ORDER BY RAND() LIMIT 3"; 
     $query_params = array(':id' => $_SESSION['user']['id']); 
         $stmt = $db->prepare($query); 
         $result = $stmt->execute($query_params); 
         $randUser = $stmt->fetchAll();
 
-    
-        
         foreach ($randUser as $row) {
             $user = $row['id'];
             $usersname = $row['username'];
