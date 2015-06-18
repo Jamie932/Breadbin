c2be1f0dd502982fb0fccdc8ba19e61f64d5c945
Jamie932 <Jamie932@gmail.com>
2015-06-18 16:31:45 +0100

    Changed directory

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
                     $site_url = "http://yourmums.science";
 
                     try{
-                        include_once "php/facebook/facebook.php";
+                        include_once dirname(__FILE__) . "/php/facebook/facebook.php";
                     }catch(Exception $e){
                         error_log($e);
                     }
