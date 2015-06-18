15c73f595b39374aef6c4b81b763b2b88003c822
Marc <BluntToothbrush>
2015-06-18 01:56:22 +0100

    Settings attempt

--- profile.php
+++ profile.php
@@ -56,6 +56,7 @@
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <script src="js/jquery-1.11.2.min.js"></script>
     <script src="js/jquery.cookie.js"></script>
+    <link rel="stylesheet" href="css/settings.css">
     <script type="text/javascript" src="js/freewall.js"></script>
     <script>
         $(document).ready(function(){
@@ -108,6 +109,16 @@
             })
             
         });
+    </script>
+    <script>
+        $(document).ready(function(){
+           $(".settingsBut").click(function(){
+               $("#rightProfile").fadeOut('normal', function(){
+                $("#settingsBox").fadeIn('normal');
+				
+           });
+		});
+       });
     </script>
 </head>
     
@@ -190,6 +201,73 @@
                 ?>
             </div>
         </div>
+        
+        <div id="settingsBox" style="height:500px;">
+        <div class="leftSettings">
+            <ul class="settingsList">
+                <li class="settingsListFirst">
+                    Account Details
+                </li>
+            <a href="privacySettings.php" class="settingsLinks">
+                <li class="settingsList">
+                    Privacy
+                </li>
+            </a>
+                <li class="settingsList">
+                    Privacy
+                </li>
+                <li class="settingsList">
+                    Privacy
+                </li>
+                <li class="settingsList">
+                    Privacy
+                </li>
+                <li class="settingsList">
+                    Privacy
+                </li>
+                <li class="settingsListLast">
+                    Privacy
+                </li>
+            </ul>
+        </div>
+        
+        <div class="rightSettings">
+            <div class="settingsField">
+                <div class="settingsHeader">
+                    <h3 class="settings">Account Details</h3>
+                    <p class="settingsDetail">Update your account details</p>
+                </div>
+                <form action="../php/SettingsUpdate.php" method="post" class="accountSettings">
+                    <label>First name: </label>
+                        <input type="text" name="firstname" class="settings" id="setFirstname" value="<?php echo $firstname ?>">
+                        <br>
+                        <br>
+                    <label>Last name: </label>
+                        <input type="text" name="lastname" class="settings" id="setLastname" value="<?php echo $lastname ?>">
+                        <br>
+                        <br>
+                    <label>Email: </label>
+                        <input type="text" name="email" class="settings" id="setEmail" value="<?php echo $email ?>">
+                        <br>
+                        <br>
+                   <label> Colour: </label>
+                        <select name="colour" class="settings" id="setColour">
+                            <option value="1" style="background:#8AE68A">Green</option>
+                            <option value="2" style="background:#6699FF">Blue</option>
+                            <option value="3" style="background:#FFB540">Orange</option>
+                            <option value="4" style="background:#FF66CC">Pink</option>
+                        </select>
+                        <br>
+                        <br>
+                    <label> </label>
+                        
+                        <input type="submit" value="Save" class="saveSettings">
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="../js/formSettings.js"></script>
+        
         <div class="clearFix"></div>
     </div>
         
