5555a319c7fffb4b16ff08ccb21ec6929565d58e
Marc <BluntToothbrush>
2015-06-18 13:42:49 +0100

    Try username

--- php/recommendToaster.php
+++ php/recommendToaster.php
@@ -1,15 +1,19 @@
 <?php 
     require("php/common.php");
 
-    $query = "SELECT count(*) FROM users"; 
+    $query = "SELECT count(*) FROM users ORDER BY RAND() LIMIT 1"; 
         $result = $db->prepare($query); 
         $result->execute();
         $usersCount = $result->fetchColumn(); 
+
+        if($usersCount){ 
+            $username = $row['username'];
+        }
 
         echo '<div class="userRecom">';
             echo '<div class="usericoRecom">';
                 echo '<img src="img/cat.jpg" height="50px" width="50px">';
             echo '</div>';
-            echo '<div class="usernameRecom">' .$usersCount . '</div>';
+            echo '<div class="usernameRecom">' .$username . '</div>';
         echo '</div>';
 ?>
