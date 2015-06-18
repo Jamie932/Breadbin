7b862bb9fb34b394f51806ced8e7365ee6cbd07d
Marc <BluntToothbrush>
2015-06-18 20:46:38 +0100

    Forgot )

--- php/toast.php
+++ php/toast.php
@@ -1,14 +1,14 @@
 <?php
     require("common.php");
 
-    $sql=$dbh->prepare("SELECT * FROM likes WHERE pid = :postID and uid= :userId");
-    $sql->execute(array(':postId' => $_POST['post'], ':userId' => $_SESSION['user']['id']);
-     $matches=$sql->rowCount();
+    $sql=$db->prepare("SELECT * FROM likes WHERE pid = :postID and uid= :userId");
+    $sql->execute(array(':postId' => $_POST['post'], ':userId' => $_SESSION['user']['id']));
+    $matches=$sql->rowCount();
                   
-     if($matches==0){
-     $sql=$dbh->prepare("INSERT INTO likes (pid, user) VALUES(:postId, :userId)");
-     $sql->execute(array(':postId' => $_POST['post'], ':userId' => $_SESSION['user']['id']));
-     $sql=$dbh->prepare("UPDATE posts SET likes=likes+1 WHERE id=:postId");
-     $sql->execute(array(':postId' => $_POST['post']));         
+    if($matches==0){
+        $sql=$db->prepare("INSERT INTO likes (pid, user) VALUES(:postId, :userId)");
+        $sql->execute(array(':postId' => $_POST['post'], ':userId' => $_SESSION['user']['id']));
+        $sql=$db->prepare("UPDATE posts SET likes=likes+1 WHERE id=:postId");
+        $sql->execute(array(':postId' => $_POST['post']));         
     }
 ?> 
