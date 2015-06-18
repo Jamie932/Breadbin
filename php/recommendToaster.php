fe501b89eb4704b55c4d82d26e93fd9387e3dd83
Marc <BluntToothbrush>
2015-06-18 14:21:51 +0100

    Try for each

--- php/recommendToaster.php
+++ php/recommendToaster.php
@@ -1,20 +1,22 @@
 <?php 
     require("php/common.php");
 
-    $query = "SELECT * FROM users ORDER BY RAND() LIMIT 1"; 
+    $query = "SELECT * FROM users ORDER BY RAND() LIMIT 3"; 
         $stmt = $db->prepare($query); 
         $result = $stmt->execute(); 
-        $row = $stmt->fetch();
+        $row = $stmt->fetchAll();
 
         if($row){ 
             $user = $row['id'];
             $usersname = $row['username'];
         }
-
-        echo '<div class="userRecom">';
-            echo '<div class="usericoRecom">';
-                echo '<img src="img/cat.jpg" height="50px" width="50px">';
+        
+        foreach ($row) {
+            echo '<div class="userRecom">';
+                echo '<div class="usericoRecom">';
+                    echo '<img src="img/cat.jpg" height="50px" width="50px">';
+                echo '</div>';
+                echo '<div class="usernameRecom"><a href="profile.php?id=' . $user . '">' . $usersname . '</a></div>';
             echo '</div>';
-            echo '<div class="usernameRecom"><a href="profile.php?id=' . $user . '">' . $usersname . '</a></div>';
-        echo '</div>';
+        }
 ?>
