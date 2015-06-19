6ebaaec9725ab0a10399c4874d02d8a525778ada
Marc <BluntToothbrush>
2015-06-19 02:10:40 +0100

    but all the bridges are too far

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -51,12 +51,14 @@
             }
             
         } else if ($row['type'] == "text") { ?>
-            <script type="text/javascript">
-            $(document).on('click','.toast', function() {                       $('.totalToasts').html(parseInt($('.totalToasts').text()) + 1);
-        $('.toast').replaceWith('<p class="untoast">Untoast</p>'); 
-                                
+
+        <script type="text/javascript">
+            $(document).on('click','.toast', function() {  
+ $('.totalToasts').html(parseInt($('.totalToasts').text()) + 1);
+        $('.toast').replaceWith('<p class="untoast">Untoast</p>');  
             })
- `          </script>
+ `      </script>
+
         <?php
             echo '<div id="contentPost" class="post-' . $row['id'] . '">';
                 echo '<div class="contentPostText">' . $row['text'] . '</div>';
