1caaf06d699fb8562c523fc8450d05c295cf7eae
Marc <BluntToothbrush>
2015-06-19 21:31:17 +0100

    badboy

--- php/post.php
+++ php/post.php
@@ -4,21 +4,6 @@
 	
 	$errors = array();
 	$data = array();
-	
-    if (empty($_POST['text']))) {
-        if (!empty($_POST['imagePost'])) {
-             $query = "INSERT INTO posts (userid, type, image)  VALUES (:userid, 'image', :image)"; 
-		$query_params = array(':userid' => $_SESSION['user']['id'], ':image' => $_POST['imagePost']); 
-	
-        $stmt = $db->prepare($query); 
-        $result = $stmt->execute($query_params);
-			
-        $data['success'] = true;
-        $data['message'] = 'Success!';
-        } else {
-            $errors['text'] = 'Error found';   
-        }
-    } else if (empty($_POST['imagePost'])) { 
                           
     if (ctype_space($_POST['ext'])) {
         $errors['text'] = 'Just spaces aren\'t allowed.';
