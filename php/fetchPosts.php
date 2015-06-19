5c88cd1ad6abae36ceb28eac5cc890485167464c
Marc <BluntToothbrush>
2015-06-19 01:46:43 +0100

    JUST DO IT PLEASE

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -55,8 +55,8 @@
             echo '$(document).on(\'click\',\'.toast\', function() {';
             echo    '$(\'.totalToasts\').html(parseInt($(\'.totalToasts\').text()) + 1);';
             echo '$(\'.toast\').replaceWith(\'<p class="untoast">Untoast</p>)'; 
-            echo '}';
-            echo '</script>';
+            echo '})';
+ `           echo '</script>';
             
             echo '<div id="contentPost" class="post-' . $row['id'] . '">';
                 echo '<div class="contentPostText">' . $row['text'] . '</div>';
