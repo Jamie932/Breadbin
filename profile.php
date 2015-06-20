722a4eb938c0381f4e86163e364458d26fa4dd7b
Marc <BluntToothbrush>
2015-06-20 16:14:48 +0100

    try that

--- profile.php
+++ profile.php
@@ -212,7 +212,7 @@
                     foreach ($posts as $row) {   
                         echo '<div class="brick size320">';
                             if ($row['type'] == "image") {
-                                echo '<img class="imagePost" src="' . $row['image'] . '">';  
+                                echo '<div class="imagePost"><img src="' . $row['image'] . '"></div>';  
                                 echo '</div>';
                             } else if ($row['type'] == "text") {
                                 echo '<p class="textPost">' . $row['text'] . '</p>';
