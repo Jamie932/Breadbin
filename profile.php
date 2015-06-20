a1e78238ab0acce926d132d094740d19a556fbb7
Marc <BluntToothbrush>
2015-06-20 16:16:28 +0100

    i hate you

--- profile.php
+++ profile.php
@@ -212,7 +212,7 @@
                     foreach ($posts as $row) {   
                         echo '<div class="brick size320">';
                             if ($row['type'] == "image") {
-                                echo '<div class="imagePost"><img src="' . $row['image'] . '"></div>';  
+                                echo '<div class="imagePost"><img style="width: 100%; height: auto;" src="' . $row['image'] . '"></div>';  
                                 echo '</div>';
                             } else if ($row['type'] == "text") {
                                 echo '<p class="textPost">' . $row['text'] . '</p>';
