52be999249de4bcb56f75963980fafd606335d3a
Marc <BluntToothbrush>
2015-06-20 15:41:30 +0100

    height

--- profile.php
+++ profile.php
@@ -212,7 +212,7 @@
                     foreach ($posts as $row) {   
                         echo '<div class="brick size320">';
                             if ($row['type'] == "image") {
-                                echo '<p class="textPost"><img src="' . $row['image'] . '"></p>';
+                                echo '<p class="textPost"><img style="width:100%; height: 100%;"src="' . $row['image'] . '"></p>';
                             } else if ($row['type'] == "text")
                             echo '<p class="textPost">' . $row['text'] . '</p>';
                         echo '</div>';
