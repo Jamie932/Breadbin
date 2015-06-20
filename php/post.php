f24e289c9001c9bd9ce9c6953661a440fd1035ba
Jamie932 <Jamie932@gmail.com>
2015-06-20 02:14:42 +0100

    I think I've fixed posting images

--- php/post.php
+++ php/post.php
@@ -1,64 +1,31 @@
 <?php 
-    header('Content-Type: text/plain; charset=utf-8');
     require("common.php"); 
 	
 	$errors = array();
 	$data = array();
 	
-    if(isset($_FILES["file"])) {
-        try {
+    if(isset($_FILES["file"]) && $_FILES["file"]["error"]== UPLOAD_ERR_OK) {
+        $updirectory = 'img/uploads/';
+        $filename = strtolower($_FILES['file']['name']);
+        $extension = substr($filename, strrpos($filename, '.'));
+        $rand = rand(0, 9999999999); 
+        $newfile = $rand.$extension;
+        
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $updirectory.$newfile )) {
+            $query = "INSERT INTO posts (userid, type, image)  VALUES (:userid, 'image', :filename)"; 
+            $query_params = array(':userid' => $_SESSION['user']['id'], ':filename' => $updirectory.$newfile); 
+
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params);
             
-        if (!isset($_FILES['file']['error']) || is_array($_FILES['file']['error'])) {
-            throw new RuntimeException('Invalid parameters.');
-        }
-
-        switch ($_FILES['file']['error']) {
-            case UPLOAD_ERR_OK:
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                throw new RuntimeException('No file sent.');
-            case UPLOAD_ERR_INI_SIZE:
-            case UPLOAD_ERR_FORM_SIZE:
-                throw new RuntimeException('Exceeded filesize limit.');
-            default:
-                throw new RuntimeException('Unknown errors.');
-        }
-
-        if ($_FILES['file']['size'] > 1000000) {
-            throw new RuntimeException('Exceeded filesize limit.');
-        }
-
-        $finfo = new finfo(FILEINFO_MIME_TYPE);
-        if (false === $ext = array_search(
-            $finfo->file($_FILES['file']['tmp_name']),
-            array(
-                'jpg' => 'image/jpeg',
-                'png' => 'image/png',
-                'gif' => 'image/gif',
-            ),
-            true
-        )) {
-            throw new RuntimeException('Invalid file format.');
-        }
-
-        // You should name it uniquely.
-        // DO NOT USE $_FILES['upfile']['name'] WITHOUT ANY VALIDATION !!
-        // On this example, obtain safe unique name from its binary data.
-        if (!move_uploaded_file($_FILES['file']['tmp_name'], sprintf('./uploads/%s.%s', sha1_file($_FILES['file']['tmp_name']), $ext))) {
-            throw new RuntimeException('Failed to move uploaded file.');
-        }
-
-        echo 'File is uploaded successfully.';
+            die('Success! File Uploaded.');
+            echo 'wooooo.';
             
-        $query = "INSERT INTO posts (userid, type, image)  VALUES (:userid, 'image', :imgurl)"; 
-        $query_params = array(':userid' => $_SESSION['user']['id'], ':imgurl' => sprintf('./uploads/%s.%s', sha1_file($_FILES['file']['tmp_name']), $ext)); 
-
-        } catch (RuntimeException $e) {
-            echo $e->getMessage();
+        } else {
+            echo 'Well shit.';
+            die('error uploading File!');
         }
-
     } else {
-        echo 'hi';
         if (empty($_POST['text'])) { 
             $errors['text'] = 'Text is required.';
         } else if (ctype_space($_POST['text'])) {
