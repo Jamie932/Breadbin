c48a4052a7444f2a05d1a016cf2e89306cefb888
Marc <BluntToothbrush>
2015-06-20 16:18:26 +0100

    trail an error at a mile an hour

--- profile.php
+++ profile.php
@@ -212,7 +212,7 @@
                     foreach ($posts as $row) {   
                         echo '<div class="brick size320">';
                             if ($row['type'] == "image") {
-                                echo '<div class="imagePost"><img style="width: 100%; height: auto;" src="' . $row['image'] . '"></div>';  
+                                echo '<p class="textPost"><img src="' . $row['image'] . '">';  
                                 echo '</div>';
                             } else if ($row['type'] == "text") {
                                 echo '<p class="textPost">' . $row['text'] . '</p>';
