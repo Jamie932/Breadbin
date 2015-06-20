165e24e319a898b5bad0611d4ddb3137ab51779f
Jamie932 <Jamie932@gmail.com>
2015-06-20 23:12:13 +0100

    Updated brick wall

--- profile.php
+++ profile.php
@@ -363,35 +363,21 @@
 	});
 
 	$(function() {
-		$(".free-wall").each(function() {
-			var wall = new freewall(this);
-			wall.reset({
-				selector: '.size320',
-				cellW: function(container) {
-					var cellWidth = 320;
-					if (container.hasClass('size320')) {
-						cellWidth = container.width()/2;
-					}
-					return cellWidth;
-				},
-				cellH: function(container) {
-					var cellHeight = 220;
-					if (container.hasClass('size320')) {
-						cellHeight = container.height()/2;
-					}
-					return cellHeight;
-				},
-				fixSize: 0,
-				gutterY: 20,
-				gutterX: 20,
-				onResize: function() {
-					wall.fitWidth();
-				}
-			})
-			wall.fitWidth();
-		});
-		$(window).trigger("resize");
-	});
+        var wall = new freewall(this);
+        wall.reset({
+            selector: '.brick',
+            animate: true,
+            cellW: 300,
+            cellH: 'auto',
+            onResize: function() {
+                wall.fitWidth();
+            }
+        });
+
+        wall.container.find('.brick img').load(function() {
+            wall.fitWidth();
+        });
+    });
 </script>
 </body>
 </html>
