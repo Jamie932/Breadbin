ef8b40b856897070e78cbec5cbc5acc12e41b679
Marc <BluntToothbrush>
2015-06-21 02:37:26 +0100

    try within

--- profile.php
+++ profile.php
@@ -199,10 +199,15 @@
         if (($row['type'] == 'image') || ($row['type'] == 'imagetext')) {
             $imgName = ltrim($row['image'], "/.");
             list($width, $height) = getimagesize($imgName);
+            
+            echo $width;
+            echo $height;
         }                    
     
             echo '<ul id="tiles">';     
                 if ($row['type'] == "image") {
+            $imgName = ltrim($row['image'], "/.");
+            list($width, $height) = getimagesize($imgName);
                     if ($width < 200) { 
                             echo '<li><img src="' . $row['image'] . '" width="300" height="200px"></li>';
                     } else {
