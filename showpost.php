3abe1493795f9dc071bf1daf4e80af53276ea7bf
Jamie932 <Jamie932@gmail.com>
2015-06-21 22:29:07 +0100

    Fixed showpost

--- showpost.php
+++ showpost.php
@@ -19,6 +19,116 @@ if (empty($_GET)) {
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <script src="js/jquery-1.11.2.min.js"></script>
     <script src="js/jquery.cookie.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(document).on('click','.delete', function() {
+                var confirmed = confirm("Are you sure you want to delete this post?");
+      
+                if (confirmed) {
+                    var postid = $(this).parent().attr('class').split('-')[1];
+
+                    var formData = {
+                        'post' : postid
+                    };
+
+                    $.ajax({
+                        type        : 'POST',
+                        url         : 'php/deletePost.php',
+                        data        : formData,
+                        dataType    : 'json',
+                        encode      : true,
+                        success:function(data) {  
+                            window.location.replace("main.php");
+                        }
+                    })
+                }
+            })
+            
+            $(document).on('click','.toast', function() {
+                var postid = $(this).parent().attr('class').split('-')[1]; 
+                var totalToasts = $(this).closest('#contentLike').children('.totalToasts');
+                var toastButton = $(this).closest('#contentLike').children('.toast');
+                var burnButton = $(this).closest('#contentLike').children('.unburn');
+                
+                var formData = {
+                    'post' : postid
+                };
+
+                $.ajax({
+                    type        : 'POST',
+                    url         : 'php/toast.php',
+                    data        : formData,
+                    dataType    : 'json',
+                    encode      : true
+                }) 
+
+                .done(function(data) {
+                     console.log(data);
+                    
+                    if (!data.success) {
+                        // Already toasted the post - error.
+                        alert("not successful soz");
+                    } else {
+                        if (data.removedBurn && data.addedToast) { // Previously toasted
+                            totalToasts.html(parseInt(totalToasts.text()) + 2);
+                            
+                            burnButton.css('color', 'black'); 
+                            burnButton.toggleClass('unburn burn');
+                        } else if (data.removedBurn || data.addedToast) {
+                            totalToasts.html(parseInt(totalToasts.text()) + 1);
+                        } else {
+                            alert("problem detected woop woop");
+                        }
+                        
+                        toastButton.css('color', 'darkgray'); 
+                        toastButton.toggleClass('toast untoast');
+                    };
+                })
+            })
+            
+            $(document).on('click','.burn', function() {
+                var postid = $(this).parent().attr('class').split('-')[1];
+                var totalToasts = $(this).closest('#contentLike').children('.totalToasts');
+                var burnButton = $(this).closest('#contentLike').children('.burn');
+                var toastButton = $(this).closest('#contentLike').children('.untoast');
+                
+                var formData = {
+                    'post' : postid
+                };
+
+                $.ajax({
+                    type        : 'POST',
+                    url         : 'php/burn.php',
+                    data        : formData,
+                    dataType    : 'json',
+                    encode      : true
+                })
+                
+                .done(function(data) {
+			         console.log(data); 
+                    
+                    if (!data.success) {
+                        // Already burnt the post - error.
+                        alert("not successful soz");
+                        
+                    } else {
+                        if (data.removedToast && data.addedBurn) { // Previously toasted
+                            totalToasts.html(parseInt(totalToasts.text()) - 2);
+                            
+                            toastButton.css('color', 'black'); 
+                            toastButton.toggleClass('untoast toast');
+                        } else if (data.removedToast || data.addedBurn) {
+                            totalToasts.html(parseInt(totalToasts.text()) - 1);
+                        } else {
+                            alert("problem detected woop woop");
+                        }
+                        
+                        burnButton.css('color', 'darkgray'); 
+                        burnButton.toggleClass('burn unburn');
+                    }
+                })
+            })
+    </script>
 </head>
     
 <body>
@@ -73,57 +183,5 @@ if (empty($_GET)) {
         </div>
         
     </div>
-
-	<script src="js/formPost.js"></script>
-</body>
-</html>
-
-
-
-<?php
-} else {
-    $query = "SELECT * FROM posts WHERE id = :id";
-    $query_params = array(
-        ':id' => intval($_GET['p'])
-    );
-    
-    $stmt   = $db->prepare($query);
-    $result = $stmt->execute($query_params);
-    $row    = $stmt->fetch();
-    
-    if ($row) {
-        $userid    = $row['id'];
-        $usersname = $row['username'];
-        $email     = $row['email'];
-        
-        if ($row['bio']) {
-            $bio = $row['bio'];
-        }
-        
-        if ($row['country']) {
-            $country = $row['country'];
-        }
-    }
-    
-    $query        = "SELECT count(*) FROM following WHERE user_no = :id";
-    $query_params = array(
-        ':id' => intval($_GET['id'])
-    );
-    
-    $result = $db->prepare($query);
-    $result->execute($query_params);
-    $noOfFollowers = $result->fetchColumn();
-    
-    $query        = "SELECT count(*) FROM following WHERE follower_id = :id";
-    $query_params = array(
-        ':id' => intval($_GET['id'])
-    );
-    
-    $result = $db->prepare($query);
-    $result->execute($query_params);
-    $noOfFollowing = $result->fetchColumn();
-}
-?>
-             
 </body>
 </html>
