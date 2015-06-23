33509bff8cf20412a7b3efa2395888639a732ac9
Marc <BluntToothbrush>
2015-06-23 20:41:24 +0100

    Shouldn't be making it mess

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -23,6 +23,13 @@
         echo '<div class="contentPostText" style="padding-top: 65px;"><center>Your boring followers haven\'t posted anything.</center></div>';
         echo '</div>';
     } else {
+        
+        if ($following == 0) {
+            echo '<div id="contentPost">';
+                echo '<div class="contentPostText" style="padding-top: 65px;"><center>You don\'t follow anyone.</center></div>';
+            echo '</div>';
+        }
+        
 	foreach ($posts as $row) {
 		$query = "SELECT * FROM users WHERE id = :id"; 
 		$query_params = array(':id' => $row['userid']); 
