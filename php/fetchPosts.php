7e6d88ffbfd720a80d92ff55e6acd807d224b59f
Marc <BluntToothbrush>
2015-06-24 14:04:52 +0100

    blu

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -22,9 +22,7 @@
         echo '<div id="contentPost">';
             echo '<div class="contentPostText" style="padding-top: 65px;"><center>Your boring toasters haven\'t posted anything.</center></div>';
         echo '</div>';
-        
     } else {
-        
         if ($following == 0) {
             echo '<div id="contentPostFollow">';
                 echo '<div class="contentPostText" style="padding-top: 65px;"><center>You don\'t follow any toasters.</center></div>';
