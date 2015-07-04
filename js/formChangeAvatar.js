3851b70d06c189562297140ee8e1d83d83b54ee1
Jamie932 <Jamie932@gmail.com>
2015-07-04 14:43:34 +0100

    Added debugs

--- js/formChangeAvatar.js
+++ js/formChangeAvatar.js
@@ -1,9 +1,11 @@
 $(document).ready(function() { 
     $('#avatarForm').submit(function(event) {
+        alert("Hello");
         var formData = new FormData();    
         var has_selected_file = $('input[type=file]').filter(function(){return $.trim(this.value) != ''}).length  > 0;
         
         if (has_selected_file) {
+            alert("yes sending this");
             var file = $('input[type=file]')[0].files[0];
             
             if (file['size'] < 2097152) {
