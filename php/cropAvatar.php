a6784f318692f0a5b390d288cd3f0045f08314af
Jamie932 <Jamie932@gmail.com>
2015-07-05 18:50:09 +0100

    Cant seem to fix the cropping

--- php/cropAvatar.php
+++ php/cropAvatar.php
@@ -54,10 +54,27 @@ if(!is_writable(dirname($output_filename))){
     );	
 }else{
     $resizedImage = imagecreatetruecolor($imgW, $imgH);
-    imagecopyresampled($resizedImage, $source_image, 0, 0, 0, 0, $imgW, $imgH, $imgInitW, $imgInitH);
-    $finished = imagecreatetruecolor($cropW, $cropH);
+	imagecopyresampled($resizedImage, $source_image, 0, 0, 0, 0, $imgW, $imgH, $imgInitW, $imgInitH);
+    // rotate the rezized image
+    $rotated_image = imagerotate($resizedImage, -$angle, 0);
+    // find new width & height of rotated image
+    $rotated_width = imagesx($rotated_image);
+    $rotated_height = imagesy($rotated_image);
+    // diff between rotated & original sizes
+    $dx = $rotated_width - $imgW;
+    $dy = $rotated_height - $imgH;
+    // crop rotated image to fit into original rezized rectangle
+	$cropped_rotated_image = imagecreatetruecolor($imgW, $imgH);
+	imagecolortransparent($cropped_rotated_image, imagecolorallocate($cropped_rotated_image, 0, 0, 0));
+	imagecopyresampled($cropped_rotated_image, $rotated_image, 0, 0, $dx / 2, $dy / 2, $imgW, $imgH, $imgW, $imgH);
+	// crop image into selected area
+	$final_image = imagecreatetruecolor($cropW, $cropH);
+	imagecolortransparent($final_image, imagecolorallocate($final_image, 0, 0, 0));
+	imagecopyresampled($final_image, $cropped_rotated_image, 0, 0, $imgX1, $imgY1, $cropW, $cropH, $cropW, $cropH);
+	// finally output png image
+	//imagepng($final_image, $output_filename.$type, $png_quality);
+	imagejpeg($final_image, $output_filename . '.jpg', $jpeg_quality);    
     
-    imagejpeg($finished, $output_filename.'.jpg', 80);
     
 	$response = Array(
 	    "status" => 'success',
