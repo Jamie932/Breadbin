c534d5eb7def6800cdf322f1255b93ff0baa5651
Marc <BluntToothbrush>
2015-07-08 16:14:56 +0100

    Try them

---
+++ discover.php
@@ -0,0 +1,29 @@
+<?php 
+    require("php/common.php");
+    require("php/checkLogin.php");
+?>
+<html>
+<head>
+    <title>Breadbin - Home</title>
+    <link href="css/navbar.css" rel="stylesheet" type="text/css">
+    <link href="css/discover.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <script src="js/jquery-1.11.2.min.js"></script>
+    <script src="js/jquery.cookie.js"></script>
+</head>
+<body>
+    <noscript>
+      <META HTTP-EQUIV="Refresh" CONTENT="0;URL=error.php">
+    </noscript>    
+    
+    <?php require('php/template/navbar.php');?>
+    
+    <div id="break"></div>
+    
+    <img src="img/cKI9AhG.jpg">
+
+	<script src="js/formPost.js"></script>
+</body>
+</html>
