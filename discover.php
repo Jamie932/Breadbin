edfe6ff75086d66b91ca30606d0b4ee854c1d156
Marc <BluntToothbrush>
2015-07-08 17:40:06 +0100

    Discover.css again

--- discover.php
+++ discover.php
@@ -6,7 +6,7 @@
 <head>
     <title>Breadbin - Home</title>
     <link href="css/navbar.css" rel="stylesheet" type="text/css">
-    <link href="css/profile.css" rel="stylesheet" type="text/css">
+    <link href="css/discover.css" rel="stylesheet" type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
     <link rel="icon" type="image/png" href="img/favicon.png" />
@@ -19,8 +19,6 @@
     </noscript>    
     
     <?php /*require('php/template/navbar.php');*/?>
-    
-    <div id="break"></div>
     
     <div id="main">
             <?php
