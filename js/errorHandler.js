5b546270cf89e472f5ddd2bb491e72aa0a031f51
Jamie932 <Jamie932@gmail.com>
2015-07-08 20:24:54 +0100

    errorHandler is erroring eep

--- js/errorHandler.js
+++ js/errorHandler.js
@@ -1,14 +1,10 @@
-$(document).ready(function(){
-    
-    function createError(errorMessage) { //Improve this later by pushing content down automatically!
-        $('#errorBar').animate({height: "35px"}, 500);
-        $('#errorBar').html("ERROR: " + errorMessage.toLowerCase());
-        
-        if ($('#profileContainer').length) {
-            $('#profileContainer').animate({marginTop: "35px"}, 500);
-        } else if ($('#center').length) {
-            $('#center').animate({paddingTop: "35px"}, 500);
-        }
+function createError(errorMessage) { //Improve this later by pushing content down automatically!
+    $('#errorBar').animate({height: "35px"}, 500);
+    $('#errorBar').html("ERROR: " + errorMessage.toLowerCase());
+
+    if ($('#profileContainer').length) {
+        $('#profileContainer').animate({marginTop: "35px"}, 500);
+    } else if ($('#center').length) {
+        $('#center').animate({paddingTop: "35px"}, 500);
     }
-    
-})
+}
