c09e00d3bcc59eef7b0a35c5fe76557c08c8173c
Marc <BluntToothbrush>
2015-07-08 17:46:00 +0100

    Re-add Navbar

--- discover.php
+++ discover.php
@@ -18,7 +18,7 @@
       <META HTTP-EQUIV="Refresh" CONTENT="0;URL=error.php">
     </noscript>    
     
-    <?php /*require('php/template/navbar.php');*/?>
+    <?php require('php/template/navbar.php'); ?>
     
     <div id="main">
             <?php
