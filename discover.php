20409e3432fa0bce74d85b6bbc8dd68b81fa2f95
Marc <BluntToothbrush>
2015-07-08 19:33:40 +0100

    Remove the limit

--- discover.php
+++ discover.php
@@ -23,7 +23,7 @@
     <div id="content">
     <div id="main">
             <?php
-                $query = "SELECT * FROM posts WHERE userid <> :id AND userid NOT IN (SELECT user_no FROM following WHERE follower_id = :id) ORDER BY RAND() LIMIT 1";
+                $query = "SELECT * FROM posts WHERE userid <> :id AND userid NOT IN (SELECT user_no FROM following WHERE follower_id = :id) ORDER BY RAND()";
                 $query_params = array(':id' => $_SESSION['user']['id']); 
                 $stmt = $db->prepare($query); 
                 $result = $stmt->execute($query_params); 
