c1ad0182da063e1d070464b2b936a429e2807375
Jamie932 <Jamie932@gmail.com>
2015-07-10 16:10:07 +0100

    Added flash to error

--- js/errorHandler.js
+++ js/errorHandler.js
@@ -1,15 +1,24 @@
-function createError(errorMessage) {
-    $('#errorBar').animate({height: "35px"}, 500, function() { $('#errorClose').animate({opacity:1}, 200); });
-    $('#errorText').html(errorMessage);
+var isError = false;
 
-    if ($('#profileContainer').length) {
-        $('#profileContainer').animate({marginTop: "35px"}, 500);
-    } else if ($('#center').length) {
-        $('#center').animate({paddingTop: "35px"}, 500);
+function createError(errorMessage) {
+    if (!isError) {
+        ifError = true;
+        $('#errorBar').animate({height: "35px"}, 500, function() { $('#errorClose').animate({opacity:1}, 200); });
+        $('#errorText').html(errorMessage);
+
+        if ($('#profileContainer').length) {
+            $('#profileContainer').animate({marginTop: "35px"}, 500);
+        } else if ($('#center').length) {
+            $('#center').animate({paddingTop: "35px"}, 500);
+        }
+    } else {
+        $('#errorBar').fadeIn(100).fadeOut(100).fadeIn(100);
+        $('#errorText').html(errorMessage);
     }
 }
 
 function clearErrors() {
+    isError = false;
     $('#errorClose').animate({opacity:0}, 200);
     $('#errorBar').animate({height: "0px"}, 500);
     $('#errorText').html("");  
