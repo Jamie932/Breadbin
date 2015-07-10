9621c37da2cee6a3214bc3169caa1a29ff59df70
Jamie932 <Jamie932@gmail.com>
2015-07-10 13:35:03 +0100

    Added clearError function

--- js/errorHandler.js
+++ js/errorHandler.js
@@ -8,3 +8,14 @@ function createError(errorMessage) { //Improve this later by pushing content dow
         $('#center').animate({paddingTop: "35px"}, 500);
     }
 }
+
+function clearError() {
+    $('#errorBar').animate({height: "0px"}, 500);
+    $('#errorBar').html("");  
+    
+    if ($('#profileContainer').length) {
+        $('#profileContainer').animate({marginTop: "0px"}, 500);
+    } else if ($('#center').length) {
+        $('#center').animate({paddingTop: "0px"}, 500);
+    }    
+}
