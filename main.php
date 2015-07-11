9328d9ca2f76911dbfad3ad8e8b284acc51dbb11
Jamie932 <Jamie932@gmail.com>
2015-07-11 22:39:21 +0100

    Added meta tag

--- main.php
+++ main.php
@@ -5,6 +5,7 @@
 <html>
 <head>
     <title>Breadbin</title>
+    <meta name="viewport" content="width=device-width" />
     <link href="css/common.css" rel="stylesheet" type="text/css">
     <link href="css/navbar.css" rel="stylesheet" type="text/css">
     <link href="css/main.css" rel="stylesheet" type="text/css">
