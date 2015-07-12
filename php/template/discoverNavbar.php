bf68b2f84e294dfe48f6da1de73c4be0165b5b84
Marcus <BluntToothbrush>
2015-07-12 20:35:14 +0100

    Test

--- php/template/discoverNavbar.php
+++ php/template/discoverNavbar.php
@@ -15,6 +15,20 @@
             }
         }
     }
+    
+    $(document).ready(function(){
+        // to fade in on page load
+        $("body").css("display", "none");
+        $("body").fadeIn(400); 
+        // to fade out before redirect
+        $('a').click(function(e){
+            redirect = $(this).attr('href');
+            e.preventDefault();
+            $('body').fadeOut(400, function(){
+                document.location.href = redirect
+            });
+        });
+    })
 </script>
 
 <?php
