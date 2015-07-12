0dbe87064da427b89e5fa01a7b43afd29ecd04b6
Jamie932 <Jamie932@gmail.com>
2015-07-12 14:37:41 +0100

    Negating horizontal scroll

--- settings.php
+++ settings.php
@@ -143,10 +143,10 @@
             $("#upfile").change(function (){
                 uploadingFile = true;
             });
-        });
             
-        $(window).scroll(function() {
-            $('.leftSettings').css('left', -$(window).scrollLeft()); 
+            $(window).scroll(function() {
+                $('.leftSettings').css('marginLeft', -$(window).scrollLeft()); 
+            });
         });
     </script>
     <script src="js/vendor/progressbar.min.js" async></script>
