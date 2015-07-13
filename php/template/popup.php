a6014d30d858a81cd7b63a2eafbf2683bbcc733f
Jamie932 <Jamie932@gmail.com>
2015-07-13 22:12:57 +0100

    Trying to fix JS

--- php/template/popup.php
+++ php/template/popup.php
@@ -1,11 +1,13 @@
 <script>
-    function createPopup(title, content) {
-        if ((title && title != '') && (content && content != '')) {
-            $('.popupTitle').text(title);
-            $('.popupContent').text(content);
-            $('#popup').fadeIn();
-        } else {
-            alert("Error making a popup");   
+    $(document).ready(function(){
+        function createPopup(title, content) {
+            if ((title && title != '') && (content && content != '')) {
+                $('.popupTitle').text(title);
+                $('.popupContent').text(content);
+                $('#popup').fadeIn();
+            } else {
+                alert("Error making a popup");   
+            }
         }
     }
 </script>
