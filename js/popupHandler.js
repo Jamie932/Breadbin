665c5d8056a531d4e049e126abd404459546c21b
Jamie932 <Jamie932@gmail.com>
2015-07-13 23:12:46 +0100

    Added few tweaks

--- js/popupHandler.js
+++ js/popupHandler.js
@@ -1,9 +1,15 @@
 var isPopup = false;
 
-function createPopup(title, content) {
+function createPopup(title, content, yesno) {
     if (title != undefined && content != undefined) {
         $('.popupTitle').html(title);
         $('.popupContent').html(content);
+            $('.popupOK').fadeIn();
+        
+        if (yesno != undefined) { //Two buttons - OK and Cancel
+            $('.popupCancel').fadeIn();
+        }
+        
         $('#popup').fadeIn();
     } else {
         alert("Error making a popup");   
