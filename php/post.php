a56da8f82c0b94340d33a9585abd5e6c8d28d44e
Jamie932 <Jamie932@gmail.com>
2015-07-13 12:20:43 +0100

    Adding new image resizing

--- php/post.php
+++ php/post.php
@@ -1,50 +1,49 @@
 <?php 
 	header("Content-Type: application/json", true);
     require("common.php"); 
+	require("vendor/ImageResize.php");
 	
     $data = array();
 
     if(isset($_FILES["file"])) { //If post has an image...
+        $imgInfo = getimagesize($_FILES['file']['tmp_name']);
+        $width = $imgInfo[0];
+        $height = $imgInfo[1];
+        
         $updirectory = 'img/uploads/' . $_SESSION['user']['id'] . '/';
         $filename = strtolower($_FILES['file']['name']);
-        $extension = substr($filename, strrpos($filename, '.'));
-        $rand = rand(0, 9999999999); 
-        $newfile = $rand.$extension;
+        $extension = '.jpg';
         
-        if ($extension == ".png") {
-            $image = imagecreatefrompng("../" . $updirectory.$newfile);
-            $bg = imagecreatetruecolor(imagesx($image), imagesy($image));
-            imagefill($bg, 0, 0, imagecolorallocate($bg, 255, 255, 255));
-            imagealphablending($bg, TRUE);
-            imagecopy($bg, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-            imagedestroy($image);
-            $quality = 50; // 0 = worst / smaller file, 100 = better / bigger file 
-            imagejpeg($bg, "../" . $updirectory . $rand . ".jpg", $quality);
-            imagedestroy($bg);
-            $converted = true;
-            $convertedfile = "../" . $updirectory . $rand . ".jpg";
-        }
-        
-        /*list($width, $height) = getimagesize($imgName);
-        
-        if (($width/$height > 0.5) && ($width/$height < 1.5) ) {
-        } else {
-            die('Error: Aspect ratio is too much.');
-            return false;   
-        }*/
-
         if (!file_exists('../img/uploads/' . $_SESSION['user']['id'])) {
             mkdir('../img/uploads/' . $_SESSION['user']['id'], 0777, true);
+        }        
+        
+        do {
+            $rand = rand(0, 9999999999); 
+            $newfile = $rand.$extension;
+        } while (file_exists('../' . $updirectory . $newfile));
+        
+        if ($width > 640) {
+            $uploadImage  = new ImageResize($_FILES['file']['tmp_name']);
+            $uploadImage->quality_jpg = 75;
+            $uploadImage->resizeToWidth(640);
+            $uploadImage->save("../" . $updirectory.$newfile);
+            
+        } else {
+            $uploadImage  = new ImageResize($_FILES['file']['tmp_name']);
+            $uploadImage->quality_jpg = 75;
+            $uploadImage->crop($width, $height);
+            $uploadImage->save("../" . $updirectory.$newfile); 
         }
         
-        if($converted && move_uploaded_file($_FILES['file']['tmp_name'], $convertedfile ) || (!$converted && move_uploaded_file($_FILES['file']['tmp_name'], "../" . $updirectory.$newfile ))) {
+        if (file_exists("../" . $updirectory.$newfile )) {
             if (isset($_POST['text'])) {    
                 $query = "INSERT INTO posts (userid, type, text, image)  VALUES (:userid, 'imagetext', :text, :image)"; 
-                $query_params = array(':userid' => $_SESSION['user']['id'], ':text' => $_POST['text'], ':image' => $converted ? $convertedfile : $updirectory.$newfile); 
+                $query_params = array(':userid' => $_SESSION['user']['id'], ':text' => $_POST['text'], ':image' => $updirectory.$newfile); 
 
             } else {
                 $query = "INSERT INTO posts (userid, type, image)  VALUES (:userid, 'image', :filename)"; 
-                $query_params = array(':userid' => $_SESSION['user']['id'], ':filename' => $converted ? $convertedfile : $updirectory.$newfile); 
+                $query_params = array(':userid' => $_SESSION['user']['id'], ':filename' => $updirectory.$newfile); 
             }
             
             $stmt = $db->prepare($query); 
