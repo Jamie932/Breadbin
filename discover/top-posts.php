4335a02264535f9c526e1b57cd5fac38be322488
Jamie932 <Jamie932@gmail.com>
2015-07-13 02:40:57 +0100

    Increased fade time

--- discover/top-posts.php
+++ discover/top-posts.php
@@ -31,7 +31,7 @@ require("../php/checkLogin.php");
         $(window).load(function() {
             $('#loader').hide();
             
-            $("#content").animate({ opacity: 1}, 600); 
+            $("#content").animate({ opacity: 1}, 1000); 
             $('#content').css("pointer-events", "auto");
         });
     });
