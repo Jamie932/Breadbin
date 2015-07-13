d9f1e9ff83dda68c0439d784ac5d0c483c8d4116
Jamie932 <Jamie932@gmail.com>
2015-07-13 13:52:31 +0100

    Changed temp url

--- php/vendor/ImageResize.php
+++ php/vendor/ImageResize.php
@@ -178,7 +178,7 @@ class ImageResize
      */
     public function getImageAsString($image_type = null, $quality = null)
     {
-        $string_temp = tempnam('', '');
+        $string_temp = '../../img/temp';
 
         $this->save($string_temp, $image_type, $quality);
 
