b22a5499928dc1e56c781b71a31398086c506732
Marcus <BluntToothbrush>
2015-07-13 02:28:58 +0100

    Remove that

--- discover/discover.php
+++ discover/discover.php
@@ -34,8 +34,7 @@ require("../php/checkLogin.php");
         $(window).load(function() {
             $('#loader').hide();
 
-            /*$('#content').animate({opacity: 1}, 600);*/
-            $("#content").fadeIn(400); 
+            $('#content').animate({opacity: 1}, 600);
             $('#content').css("pointer-events", "auto");
         });
     });
