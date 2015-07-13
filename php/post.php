e10f7872b67c54e17b21de5c0eeba490841d4fbc
Jamie932 <Jamie932@gmail.com>
2015-07-13 13:08:01 +0100

    Upped image quality

--- php/post.php
+++ php/post.php
@@ -25,13 +25,13 @@
         
         if ($width > 640) {
             $uploadImage  = new ImageResize($_FILES['file']['tmp_name']);
-            $uploadImage->quality_jpg = 80;
+            $uploadImage->quality_jpg = 90;
             $uploadImage->resizeToWidth(640);
             $uploadImage->save("../" . $updirectory.$newfile, IMAGETYPE_JPEG);
             
         } else {
             $uploadImage  = new ImageResize($_FILES['file']['tmp_name']);
-            $uploadImage->quality_jpg = 80;
+            $uploadImage->quality_jpg = 90;
             $uploadImage->crop($width, $height);
             $uploadImage->save("../" . $updirectory.$newfile, IMAGETYPE_JPEG); 
         }
