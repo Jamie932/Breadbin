0d8300ac2b4d4ec2498f6d8d8f536af4cb3b8794
Jamie932 <Jamie932@gmail.com>
2015-07-13 16:20:50 +0100

    Moved progress back

--- main.php
+++ main.php
@@ -172,7 +172,6 @@
                     <div class="textarea">
                         <textarea name="TextUpload" class="postText" id="uploadText" maxlength="150" placeholder="Write a slice..."></textarea>
                     </div>
-                    <div id="progress"></div>
                     
                     <div class="uploadRest">
                         <input type="submit" value="Submit" id="submitPost" class="buttonstyle">
@@ -186,6 +185,7 @@
             <div class="clearFix"></div>
             
             <?php require('php/recommendToaster.php');  ?>
+            <div id="progress"></div>
         </div>
         
         <div id="content">
