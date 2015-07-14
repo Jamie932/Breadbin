1504b3fa7ae4f0a49e2cef7898e008efe747761a
Jamie932 <Jamie932@gmail.com>
2015-07-14 21:34:26 +0100

    Check rank against database

--- php/checkRank.php
+++ php/checkRank.php
@@ -1,5 +1,14 @@
 <?php 
-    if (empty($_SESSION['user']['rank']) || ($_SESSION['user']['rank'] == "user")) {
-        header('Location: errorFile.php?error=403');
+    $query = "SELECT id, rank FROM users WHERE id = :userid"; 
+    $query_params = array(':userid' => $_SESSION['user']['id']); 
+
+    $stmt = $db->prepare($query); 
+    $result = $stmt->execute($query_params); 
+    $row = $stmt->fetch();
+
+    if ($row) {
+        if (empty($row['rank']) || $row['rank'] == "user") {
+            header('Location: errorFile.php?error=403');
+        }
     }
 ?> 
