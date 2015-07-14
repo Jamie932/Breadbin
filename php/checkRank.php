169d1a9249bf911b7bd5d604d58612b8eecb6e0c
Jamie932 <Jamie932@gmail.com>
2015-07-14 20:25:34 +0100

    Redirect shizz

--- php/checkRank.php
+++ php/checkRank.php
@@ -1,6 +1,6 @@
 <?php 
     if (empty($_SESSION['user']['rank']) || ($_SESSION['user']['rank'] == "user")) {
         header('HTTP/1.0 403 Forbidden');
-        include('errorFile.php');
+        header('Location: errorFile.php');
     }
 ?> 
