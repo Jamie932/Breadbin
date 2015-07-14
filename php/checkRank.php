bacd77b3e25480fe8c155c2efc89ef6bbc2654cf
Jamie932 <Jamie932@gmail.com>
2015-07-14 20:03:38 +0100

    Trying new header redirect

--- php/checkRank.php
+++ php/checkRank.php
@@ -1,5 +1,5 @@
 <?php 
     if (empty($_SESSION['user']['rank']) || ($_SESSION['user']['rank'] == "user")) {
-        header("X-PHP-Response-Code: 403", true, 403);
+        header(' ', true, 403);
     }
 ?> 
