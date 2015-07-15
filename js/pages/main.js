4a3700801321d15e5f854c05903179fcd7d23116
Marcus <BluntToothbrush>
2015-07-15 23:03:45 +0100

    main.js update

--- js/pages/main.js
+++ js/pages/main.js
@@ -68,19 +68,30 @@ $(document).ready(function(){
     });    
 
     $('#postRecipeForm').submit(function(event) {
-        var formData = {
-            'title' : $('#recipeTitle').val(),
-            'ingredients' : $('#recipeIngredients').val(),
-            'instructions' : $('#recipeInstructions').val()
-        };
+               
+            
+                var newArray = new Array();
 
-        $.ajax({
-            type        : 'POST',
-            url         : 'php/postRecipe.php',
-            data        : formData,
-            dataType    : 'json',
-            encode      : true
-        })
+                $('#recipeIngredients').each(function(){
+                    newArray.push($(this));
+                })
+        
+                    var formData = {
+                        'title' : $('#recipeTitle').val(),
+                        'ingredients' : $('#recipeIngredients').val(),
+                        'instructions' : $('#recipeInstructions').val()
+                    };
+
+                    $.ajax({
+                        type        : 'POST',
+                        url         : 'php/postRecipe.php',
+                        data        : formData,
+                        dataType    : 'json',
+                        encode      : true
+                    })
+                    
+        event.preventDefault();
+        
     })
 
     $(".hide").click(function(){
