5319ee7b38627d2746eebee6f9a7c4c745f45d48
Jamie932 <Jamie932@gmail.com>
2015-07-15 15:23:59 +0100

    Added colour saving

--- js/formSettings.js
+++ js/formSettings.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {  
-    
     $('#detailsForm').submit(function(event) {
+        var colourSelection = ($('.depressedColour').hasClass('orange') ? '1' : false) || ($('.depressedColour').hasClass('blue') ? '2' : false) || 
+            ($('.depressedColour').hasClass('green') ? '3' : false) || ($('.depressedColour').hasClass('red') ? '4' : false) ||
+            ($('.depressedColour').hasClass('purple') ? '5' : false) || ($('.depressedColour').hasClass('pink') ? '6' : false);        
+        
         var formData = {
             'firstname' : $('#setFirstname').val(),
             'lastname' : $('#setLastname').val(),
@@ -8,6 +11,8 @@ $(document).ready(function() {
             'colour' : $('#setColour').val()
         };
 
+        
+        
         $.ajax({
             type        : 'POST',
             url         : '../php/SettingsUpdate.php',
