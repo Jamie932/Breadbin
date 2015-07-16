9eb29ebdb602ecdd3e1549aa23c4f68daabe5a3e
Marcus <BluntToothbrush>
2015-07-16 15:26:28 +0100

    Can't discover your own posts

--- discover/just-text.php
+++ discover/just-text.php
@@ -75,7 +75,7 @@
     <div id="content">
         <div id="main">
             <?php
-            $query        = "SELECT * FROM posts WHERE type = 'text' AND userid NOT IN (SELECT user_no FROM following WHERE follower_id = :id) ORDER BY RAND()";
+            $query        = "SELECT * FROM posts WHERE userid <> :id AND type = 'text' AND userid NOT IN (SELECT user_no FROM following WHERE follower_id = :id) ORDER BY RAND()";
             /*userid <> :id AND */
             $query_params = array(':id' => $_SESSION['user']['id']);
             $stmt         = $db->prepare($query);
