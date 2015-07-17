b381ffc262755b52485d5f998e2e929f7287dd87
Jamie <Jamie932@gmail.com>
2015-07-17 16:17:24 +0100

    Fixed conflicting title CSS

--- php/template/navbar.php
+++ php/template/navbar.php
@@ -59,7 +59,7 @@
     ?>
 
 	<div class="left">
-		<a href="/main.php" class="navLinks title" style="float: left;">Bread Bin</a>
+		<a href="/main.php" id="navTitle" class="navLinks" style="float: left;">Bread Bin</a>
         
 		<div id="searchContainer">
 			<form class="searchForm">
