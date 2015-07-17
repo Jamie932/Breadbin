6ecd5e598015a415ef1f670f850379ad4aa877e7
Marcus <BluntToothbrush>
2015-07-17 13:41:48 +0100

    Update recipe box

--- main.php
+++ main.php
@@ -39,7 +39,13 @@
         
     <div id="break"></div>
         
-    <div id="recipeBox">
+    <script>
+        function add_fieldsInstruc() {
+            document.getElementById('instructionsPost').innerHTML += '<textarea name="TextUpload" class="recipeInstructions" id="recipeInstructions" maxlength="180" placeholder="Recipe Instructions..."></textarea>';
+        }
+    </script>
+        
+    <div id="recipeBox" style="display: inline;">
             <div class="recipeContent"> 
                 <div class="recipeHeader">
                     <h1>Post your recipe</h1>
@@ -57,8 +63,12 @@
                             <input type="text" name="recipeIngredients" id="recipeIngredients" placeholder="Recipe Ingredient" class="recipeIngredients" style="margin-right: -4px;" required/>
                         </div>
                             <center><input type="button" id="more_fields" onclick="add_fields();" value="New Ingredient" /></center>
+                        
                         <h3>Recipe Instructions</h3>
-                            <center><textarea name="TextUpload" class="recipeInstructions" id="recipeInstructions" maxlength="150" placeholder="Recipe Instructions..."></textarea></center>
+                        <div id="instructionsPost">
+                            1 - <textarea name="TextUpload" class="recipeInstructions" id="recipeInstructions" maxlength="220" placeholder="Recipe Instructions..."></textarea>
+                        </div>
+                            <center><input type="button" id="more_fields" onclick="add_fieldsInstruc();" value="New Ingredient" /></center>
                             <center><input type="submit" value="Submit" id="submitRecipe" class="buttonstyle">
                             <input type="button" id="cancel" class="buttonstyle" value="Cancel" /></center>
                     </form>
