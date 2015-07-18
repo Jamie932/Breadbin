66fc63287a580bd981a456fad7d6ec02b24bb71c
Marcus <BluntToothbrush>
2015-07-18 19:07:25 +0100

    Reduce the minus

--- main.php
+++ main.php
@@ -96,7 +96,7 @@
                         </div>
                         <div id="ingredientBody">
                             <input type="text" name="recipeIngredients" id="recipeIngredients" placeholder="Recipe Ingredient" class="recipeIngredients" required/>
-                            <input type="text" name="recipeIngredients" id="recipeIngredients" placeholder="Recipe Ingredient" class="recipeIngredients" style="margin-left: -4px;" required/>
+                            <input type="text" name="recipeIngredients" id="recipeIngredients" placeholder="Recipe Ingredient" class="recipeIngredients" style="margin-left: -3px;" required/>
                         </div>
                         
                         <div id="sectionHeader" style="margin-bottom:3px;">
