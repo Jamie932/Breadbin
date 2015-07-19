576fe6349139c3cba27f37fc1f713b137f453ec6
Jamie <Jamie932@gmail.com>
2015-07-19 12:11:08 +0100

    Removed bold font to reduce page load

--- burned.php
+++ burned.php
@@ -11,97 +11,14 @@
     <link href="css/navbar.css" rel="stylesheet" type="text/css">
     <link href="css/toasted.css" rel="stylesheet" type="text/css">
     <link href="css/vendor/normalize.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <script src="js/vendor/jquery-1.11.2.min.js"></script>
     <script src="js/vendor/jquery.cookie.js"></script>
+    <script src="js/postFunctions.js" async></script>
     <script src="js/errorHandler.js" async></script>
     <script async>
-            $(document).on('click','.toast', function() {
-                var postid = $(this).parent().attr('class').split('-')[1]; 
-                var totalToasts = $(this).closest('#contentLike').children('.totalToasts');
-                var toastButton = $(this).closest('#contentLike').children('.toast');
-                var burnButton = $(this).closest('#contentLike').children('.unburn');
-                
-                var formData = {
-                    'post' : postid
-                };
-
-                $.ajax({
-                    type        : 'POST',
-                    url         : 'php/toast.php',
-                    data        : formData,
-                    dataType    : 'json',
-                    encode      : true
-                }) 
-
-                .done(function(data) {
-                     console.log(data);
-                    
-                    if (!data.success) {
-                        // Already toasted the post - error.
-                        createError("This post has already been toasted by you."); 
-                    } else {
-                        if (data.removedBurn && data.addedToast) { // Previously toasted
-                            totalToasts.html(parseInt(totalToasts.text()) + 2);
-                            
-                            burnButton.css('color', 'black'); 
-                            burnButton.toggleClass('unburn burn');
-                        } else if (data.removedBurn || data.addedToast) {
-                            totalToasts.html(parseInt(totalToasts.text()) + 1);
-                        } else {
-                            createError("Incorrect toast data returned. Please inform an adminstrator."); 
-                        }
-                        
-                        toastButton.css('color', 'darkgray'); 
-                        toastButton.toggleClass('toast untoast');
-                    };
-                })
-            })
-            
-            $(document).on('click','.burn', function() {
-                var postid = $(this).parent().attr('class').split('-')[1];
-                var totalToasts = $(this).closest('#contentLike').children('.totalToasts');
-                var burnButton = $(this).closest('#contentLike').children('.burn');
-                var toastButton = $(this).closest('#contentLike').children('.untoast');
-                
-                var formData = {
-                    'post' : postid
-                };
-
-                $.ajax({
-                    type        : 'POST',
-                    url         : 'php/burn.php',
-                    data        : formData,
-                    dataType    : 'json',
-                    encode      : true
-                })
-                
-                .done(function(data) {
-			         console.log(data); 
-                    
-                    if (!data.success) {
-                        // Already burnt the post - error.
-                         createError("This post has already been burnt by you."); 
-                    } else {
-                        if (data.removedToast && data.addedBurn) { // Previously toasted
-                            totalToasts.html(parseInt(totalToasts.text()) - 2);
-                            
-                            toastButton.css('color', 'black'); 
-                            toastButton.toggleClass('untoast toast');
-                        } else if (data.removedToast || data.addedBurn) {
-                            totalToasts.html(parseInt(totalToasts.text()) - 1);
-                        } else {
-                            createError("Incorrect burn data returned. Please inform an adminstrator."); 
-                        }
-                        
-                        $(postid).animate({height: 0, opacity: 0, marginBottom: 0}, 600, function() { $(this).remove();});
-                        burnButton.css('color', 'darkgray'); 
-                        burnButton.toggleClass('burn unburn');
-                    }
-                });
-            })
             $(document).ready(function(){
                 $(function(){
                   $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
