dcb3ffc72a243abc9b53c35a6ac0d703386907e5
Marcus <BluntToothbrush>
2015-07-20 15:49:48 +0100

    Can view top posts

--- discover.php
+++ discover.php
@@ -83,7 +83,8 @@
 				if ($_GET['f'] == 1) { //Staff Recommended
 					$query = "SELECT * FROM posts WHERE userid <> :id AND favourite = 1 AND userid NOT IN (SELECT user_no FROM following WHERE follower_id = :id) ORDER BY RAND()";
 				} else if ($_GET['f'] == 2) { //Top Posts
-					$query = "SELECT posts.*, COUNT(post_toasts.userid) AS toasts, COUNT(post_burns.userid) AS burns, (COUNT(post_toasts.userid) - COUNT(post_burns.userid)) AS total FROM posts LEFT JOIN post_toasts ON post_toasts.postid = posts.id LEFT JOIN post_burns ON post_burns.postid = posts.id WHERE posts.userid <> :id AND posts.userid NOT IN (SELECT user_no FROM following WHERE follower_id = :id) GROUP BY posts.id HAVING (total) > 0 ORDER BY total";
+					$query = "SELECT posts.*, COUNT(post_toasts.userid) AS toasts, COUNT(post_burns.userid) AS burns, (COUNT(post_toasts.userid) - COUNT(post_burns.userid)) AS total FROM posts LEFT JOIN post_toasts ON post_toasts.postid = posts.id LEFT JOIN post_burns ON post_burns.postid = posts.id WHERE posts.userid <> :id GROUP BY posts.id HAVING (total) > 0 ORDER BY total";
+                    /*AND posts.userid NOT IN (SELECT user_no FROM following WHERE follower_id = :id)*/
 				} else if ($_GET['f'] == 3) { //Just Pictures
 					$query = "SELECT * FROM posts WHERE userid <> :id AND type = 'image' AND userid NOT IN (SELECT user_no FROM following WHERE follower_id = :id) ORDER BY RAND()";
 				} else if ($_GET['f'] == 4) {
