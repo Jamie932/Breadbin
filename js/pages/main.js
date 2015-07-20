6f453fda6bc19a197e618124565f18f3d89cf814
Marcus <BluntToothbrush>
2015-07-20 15:42:43 +0100

    What's it doing

--- js/pages/main.js
+++ js/pages/main.js
@@ -158,8 +158,12 @@ $(document).ready(function(){
         $('.bodyHalf').on('keydown', '#recipePrepTime', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
     });
     
-    $("#recipeTime").keydown(function (e) {
+    $("#recipePrepTime").keydown(function (e) {
         $('.bodyHalf').on('keydown', '#recipeTime', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
+    });
+    
+    $("#recipeTime").keydown(function (e) {
+        $('.bodyHalfServe').on('keydown', '#recipeServe', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
     });
 
 function getFile(){
