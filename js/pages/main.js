3e2d540c44df12be3b7b87464498e7daf5f35f46
Marcus <BluntToothbrush>
2015-07-20 16:10:16 +0100

    Missed a )};

--- js/pages/main.js
+++ js/pages/main.js
@@ -155,7 +155,7 @@ $(document).ready(function(){
     });
 });
     
-    /*$("#recipePrepTime").keydown(function (e) {
+    $("#recipePrepTime").keydown(function (e) {
         $('.bodyHalf').on('keydown', '#recipePrepTime', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
     });
     
@@ -165,7 +165,7 @@ $(document).ready(function(){
     
     $("#recipeTime").keydown(function (e) {
         $('.bodyHalfServe').on('keydown', '#recipeServe', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
-    });*/
+    });
 
 function getFile(){
     $('#upfile').click();
