de45959a7b995bac455c6ff18e92a113d24e5f1e
Jamie <Jamie932@gmail.com>
2015-07-26 20:21:19 +0100

    Adding a search

---
+++ php/fetchSearch.php
@@ -0,0 +1,49 @@
+<?php
+    require("common.php"); 
+	require("vendor/timeago.php");
+	
+	if (!isset($_GET['q'])) {
+        header('Location: main.php');
+        die();
+	}
+	
+    $query= "SELECT * FROM users WHERE username LIKE '%:query%' OR firstname LIKE '%:query%' OR lastname LIKE '%:query%' LIMIT 10";
+    $query_params = array(':query' => $_GET['q']);
+    $stmt = $db->prepare($query); 
+    $result = $stmt->execute($query_params); 
+	$users = $stmt->fetchAll();
+
+    $query= "SELECT * FROM posts WHERE text LIKE '%:query%' LIMIT 10";
+    $query_params = array(':query' => $_GET['q']);
+    $stmt = $db->prepare($query); 
+    $result = $stmt->execute($query_params); 
+	$posts = $stmt->fetchAll();
+
+    $query= "SELECT * FROM following WHERE follower_id = :userId"; 
+    $query_params = array(':userId' => $_SESSION['user']['id']);
+    $stmt = $db->prepare($query); 
+    $result = $stmt->execute($query_params); 
+	$following = $stmt->rowCount();
+
+	if (!$users) {
+		echo 'No search results found.';
+		die();
+	} 
+
+	echo '<div id="usersBox">';
+	echo '<div class="boxTitle"><b>3</b> Users Found <div class="expand"><i class="fa fa-angle-double-down"></i></div></div>';
+
+	foreach ($users as $row) {	
+			echo '<div class="user">';
+				echo '<div class="userImage"></div>';
+				echo '<div class="userInfo">';
+					echo '<div class="userTitle">' . $row["username"] . '</div>';
+					echo '<div class="userCountry">' . $row["country"] . '</div>';
+					echo '<div class="userButtons"><input type="submit" value="Follow" class="buttonstyle"><input type="submit" value="Message" class="buttonstyle"></div>';
+					echo '<div class="userBio">' . $row["bio"] . '</div>';
+				echo '</div>';
+			echo '</div>';
+	}
+
+	echo '</div>';
+?>
---
+++ search.php
@@ -0,0 +1,49 @@
+<?php 
+    require("php/common.php");
+    require("php/checkLogin.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search | Breadbin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="css/common.css" rel="stylesheet" type="text/css">
+    <link href="css/navbar.css" rel="stylesheet" type="text/css">
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+    <link href="css/search.css" rel="stylesheet" type="text/css">
+    <link href="css/vendor/normalize.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <script src="js/vendor/jquery-1.11.2.min.js"></script>
+    <script src="js/vendor/jquery.cookie.js"></script>
+    <script src="js/pages/main.js"></script>
+    <script src="js/postFunctions.js"></script>
+    <script src="js/errorHandler.js"></script>
+    <script src="js/vendor/progressbar.min.js"></script>
+</head> 
+<body>
+    <noscript>
+      <META HTTP-EQUIV="Refresh" CONTENT="0;URL=error.php">
+    </noscript>    
+		
+    <?php require('php/template/navbar.php');?>
+    <?php require('php/template/popup.php');?>
+        
+    <div id="break"></div>
+    
+    <div id="center">  
+        <div id="content">
+            <ul id="images">
+				<?php require('php/fetchSearch.php');?>
+            </ul>
+        </div>
+        
+        <div id="sidebar">
+            <div id="support" class="sideBox">
+				About Support etc etc
+			</div>
+        </div>        
+    </div>
+</body>
+</html>
