147a9e8a4df6f52d49011bd0e89b45136dd36469
Marcus <BluntToothbrush>
2015-07-27 22:00:05 +0100

    Revert back

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -230,9 +230,7 @@
                 
                 echo '<div id="contentPost" class="post-' . $row['id'] . '">';
                 echo $row['favourite'] ? '<div id="heart"><i class="fa fa-heart"></i></div><div class="contentPostImage imgNoPadding favouriteImg">' : '<div class="contentPostVideo imgNoPadding">';
-                echo '<div class="js-lazyYT" data-youtube-id="'.$row['text'].'" data-width="640px" data-height="361px"></div>';
-                echo '</div>';
-                echo '</div>';
+                echo '<div class="js-lazyYT" data-youtube-id="'.$row['text'].'" data-width="640px" data-height="361px"></div></div>';
                 
             }
                 echo '<div id="contentInfoText">';
