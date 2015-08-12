e384a9b7229b5a2b7800532fd6f9443347694ff6
Marcus <BluntToothbrush>
2015-08-12 20:20:04 +0100

    font-size 4.7em

--- php/template/gridPopUp.php
+++ php/template/gridPopUp.php
@@ -6,7 +6,7 @@
             </div>
             <div class="gridBoxes 1"></div>
 		  <div class="gridBoxes 2">
-                <span class="fa-stack fa-lg fa-4x">
+                <span class="fa-stack fa-lg" style="font-size: 4.7em">
                   <i class="fa fa-square-o fa-stack-2x"></i>
                     <i class="fa fa-camera fa-stack-1x"></i>
                 </span>
