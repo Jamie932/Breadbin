4308fd93abdd870367867946afbd0dd68128e035
Marcus <BluntToothbrush>
2015-08-12 16:01:22 +0100

    Scroll disable

--- js/pages/main.js
+++ js/pages/main.js
@@ -1,3 +1,37 @@
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
+
 $(document).ready(function(){
    $('#postForm').submit(function(event) {
         var formData = new FormData();    
@@ -174,6 +208,7 @@ $(document).ready(function(){
     });
     
     $("#gridClick").click(function(){
+        disableScroll();
         $('#blackOverlay').fadeIn('normal');
         $('#gridBox').fadeIn('normal');
     });
@@ -198,6 +233,7 @@ $(document).ready(function(){
             $('.innerRecipe').hide();
             $('.innerGrid').show();
         })
+        enableScroll();
     });
     
     $("#rightTitleRec").click(function(){
@@ -206,6 +242,7 @@ $(document).ready(function(){
             $('.innerRecipe').hide();
             $('.innerGrid').show();
         })
+        enableScroll();
     });
 
     $(".recBack").click(function(){
