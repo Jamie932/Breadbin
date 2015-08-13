e8a98b90ed5135be4f87429db08d7052d515df7d
Marcus <BluntToothbrush>
2015-08-13 12:33:49 +0100

    Change Log in -> submit

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 					<input type="text" name="log_username" placeholder="Username" class="logUserName textField" required/> 
 					<input type="password" name="log_password" placeholder="Password" class="logUserPass textField" required/> 
 						
-					<input type="submit" class="btn btn-info" value="Submit" id="submit"/>					
+					<input type="Log in" class="btn btn-info" value="Submit" id="submit"/>					
 				</form>
 				<div class="forgotPass"><a>Forgotten your password?</a></div>
 			</div>
