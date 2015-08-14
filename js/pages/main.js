0f959e78e7ddd87ea93684a80f9ebc7c4762c158
Jamie <Jamie932@gmail.com>
2015-08-14 13:48:35 +0100

    Added slight height offset

--- js/pages/main.js
+++ js/pages/main.js
@@ -34,7 +34,7 @@ function enableScroll() {
 
 $(document).ready(function(){
 	$('#uploadText').each(function () {
-		this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+		this.setAttribute('style', 'height:' + (this.scrollHeight + 2) + 'px;overflow-y:hidden;');
 	}).on('input', function () {
 	  	this.style.height = 'auto';
 	  	this.style.height = (this.scrollHeight) + 'px';
