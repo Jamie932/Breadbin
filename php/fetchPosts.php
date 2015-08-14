a6f60923e0dd4ff1085581d749d58c5ab4d6a0ea
Marcus <BluntToothbrush>
2015-08-14 14:04:02 +0100

    Missed videos

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -257,8 +257,14 @@
                 echo '</div>';
                 echo '</div>';
             } else {
-                
                 echo '<div id="contentPost" class="post-' . $row['id'] . '">';
+                echo '<div id="leftUserImg">';
+                    if (!file_exists('img/avatars/' . $row['userid'] . '/avatar.jpg')) {
+                        echo '<img src="img/profile2.png" height="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    } else { 
+                        echo '<img src="img/avatars/' . $row['userid'] . '/avatar.jpg" height="50px" width="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    }
+                echo '</div>';
                 echo $row['favourite'] ? '<div id="heart"><i class="fa fa-heart"></i></div><div class="contentPostImage imgNoPadding favouriteImg">' : '<div class="contentPostVideo imgNoPadding">';
                 echo '<div class="js-lazyYT" data-youtube-id="'.$row['text'].'" data-width="640px" data-height="361px"></div></div>';
                 
