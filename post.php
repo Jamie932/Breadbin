f05c72cf7b6f207eb83a8fb9749d17732bff80ee
Marcus <BluntToothbrush>
2015-08-14 16:33:11 +0100

    stab in the dark

--- post.php
+++ post.php
@@ -6,6 +6,12 @@
     $stmt = $db->prepare($query); 
     $row = $stmt->fetch();
 
+    $query        = "SELECT * FROM posts WHERE id = :id";
+    $query_params = array(':id' => intval($_GET['id']));
+    $stmt   = $db->prepare($query);
+    $result = $stmt->execute($query_params);
+    $row    = $stmt->fetch();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -45,6 +51,279 @@
         
     
     <div id="center">
+        
+        $query = "SELECT * FROM users WHERE id = :id"; 
+            $query_params = array(':id' => $row['userid']); 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+
+            $userrow = $stmt->fetch();
+            $username = 'Unknown';
+
+            $query = "SELECT * FROM post_burns WHERE postid = :postId AND userid= :userId"; 
+            $query_params = array(':postId' => $row['id'], ':userId' => $_SESSION['user']['id']); 
+
+            $stmt = $db->prepare($query);
+            $result = $stmt->execute($query_params); 
+            $ifBurnt = $stmt->rowCount();
+
+            $query = "SELECT * FROM post_toasts WHERE postid = :postId AND userid = :userId"; 
+            $query_params = array(':postId' => $row['id'], ':userId' => $_SESSION['user']['id']);
+            $stmt = $db->prepare($query);
+            $result = $stmt->execute($query_params); 
+            $ifToasted = $stmt->rowCount();
+
+            $totalToasts = $row['toasts'] - $row['burns'];
+
+            if($userrow){ 
+                $username = $userrow['username'];
+            } else {
+                $query = "DELETE FROM posts WHERE userid = :userid"; 
+                $query_params = array(':userid' => $row['userid']); 
+
+                $stmt = $db->prepare($query); 
+                $result = $stmt->execute($query_params); 
+            }
+
+            if (($row['type'] == 'image') || ($row['type'] == 'imagetext')) {
+                $img = (basename(getcwd()) == "php") ? '../' . $row['image'] : $row['image'];
+                
+                /*if (!file_exists($img)) {
+                    $query = "DELETE FROM posts WHERE id = :id"; 
+                    $query_params = array(':id' => $row['id']); 
+
+                    $stmt = $db->prepare($query); 
+                    $result = $stmt->execute($query_params); 
+                    
+                    echo '<script>window.location.reload()</script>';
+                    
+                }*/
+                
+                list($width, $height) = getimagesize($img);
+
+                if ($width > 600) {
+                    $class = 'imgNoPadding';
+                } else {
+                    $class = 'imgPadding';
+                }
+            }
+
+            if (($row['type'] == 'text') || ($row['type'] == 'imagetext')) {
+                if (preg_match_all('/(?<!\w)@(\w+)/', $row['text'], $matches)) {
+                    $users = $matches[1];
+
+                    foreach ($users as $user) {
+                        $query = "SELECT id, username FROM users WHERE username = :username"; 
+                        $query_params = array(':username' => $user); 
+
+                        $stmt = $db->prepare($query); 
+                        $result = $stmt->execute($query_params);
+                        $userFound = $stmt->fetch(); 
+
+                        if ($userFound) {
+                            $row['text'] = str_replace('@' .$user, '<a href="profile.php?id=' .$userFound['id'] . '">' . $user . '</a>', $row['text']);
+                        }
+
+                    }
+                }
+            }
+
+            echo '<div id="post">';
+
+            if ($row['type'] == "imagetext") {
+                echo '<div id="contentPost" class="post-' . $row['id'] . '">';
+                echo '<div id="leftUserImg">';
+                    echo '<a href="profile.php?id=' . $row['userid'] . '">';
+                    if (!file_exists('img/avatars/' . $row['userid'] . '/avatar.jpg')) {
+                        echo '<img src="img/profile2.png" height="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    } else { 
+                        echo '<img src="img/avatars/' . $row['userid'] . '/avatar.jpg" height="50px" width="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    }
+                    echo '</a>';
+                echo '</div>';
+                echo $row['favourite'] ? '<div id="heart"><i class="fa fa-heart" style="cursor: default;"></i></div><div class="contentPostImage ' . $class . ' favouriteImg">' : '<div class="contentPostImage ' . $class . '">';
+                echo '<img src="' . $img . '"><div class="imgtext">' . $row['text'] . '</div></div>';
+            } else if ($row['type'] == "image") {
+                echo '<div id="contentPost" class="post-' . $row['id'] . '">';
+                echo '<div id="leftUserImg">';
+                    echo '<a href="profile.php?id=' . $row['userid'] . '">';
+                    if (!file_exists('img/avatars/' . $row['userid'] . '/avatar.jpg')) {
+                        echo '<img src="img/profile2.png" height="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    } else { 
+                        echo '<img src="img/avatars/' . $row['userid'] . '/avatar.jpg" height="50px" width="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    }
+                    echo '</a>';
+                echo '</div>';
+                echo $row['favourite'] ? '<div id="heart"><i class="fa fa-heart" style="cursor: default;"></i></div><div class="contentPostImage ' . $class . ' favouriteImg">' : '<div class="contentPostImage ' . $class . '">';
+                echo '<img src="' . $img . '"></div>';
+            } else if ($row['type'] == "text") {
+                echo '<div id="contentPost" class="post-' . $row['id'] . '">';
+                echo '<div id="leftUserImg">';
+                    echo '<a href="profile.php?id=' . $row['userid'] . '">';
+                    if (!file_exists('img/avatars/' . $row['userid'] . '/avatar.jpg')) {
+                        echo '<img src="img/profile2.png" height="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    } else { 
+                        echo '<img src="img/avatars/' . $row['userid'] . '/avatar.jpg" height="50px" width="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    }
+                    echo '</a>';
+                echo '</div>';
+                echo $row['favourite'] ? '<div id="heart"><i class="fa fa-heart" style="cursor: default;"></i></div><div class="contentPostText favouriteText">' : '<div class="contentPostText">';
+                echo '<p style="margin: 0;">' . $row['text'] . '</p></div>';
+            } else if ($row['type'] == "recipe") {
+                $instrucNo = 0;
+                echo '<div id="contentPost" class="post-' . $row['id'] . '">';
+                echo '<div id="leftUserImg">';
+                    echo '<a href="profile.php?id=' . $row['userid'] . '">';
+                    if (!file_exists('img/avatars/' . $row['userid'] . '/avatar.jpg')) {
+                        echo '<img src="img/profile2.png" height="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    } else { 
+                        echo '<img src="img/avatars/' . $row['userid'] . '/avatar.jpg" height="50px" width="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    }
+                    echo '</a>';
+                echo '</div>';
+                echo $row['favourite'] ? '<div id="heart"><i class="fa fa-heart" style="cursor: default;"></i></div><div class="contentPostText favouriteText">' : '<div class="contentPostText">';
+                echo '<div class="recTitle">';
+                echo '<h3 class="recTit">' .$row['title']. '</h3>';
+                echo '</div>';
+                echo '<div class="timeServe">';
+                
+                $prepArray = json_decode($row['prepTime']);
+                
+                $prepTimeNo = 0;
+                $prepTot = count($prepArray);
+                
+                echo '<p class="times" style="margin-right: 50px;"><b>Prep time:</b>';
+                foreach ($prepArray as $value) {
+                    $prepTimeNo++;
+                    if ($prepTimeNo == 1 && $value >= 1) {
+                        if ($value == 1) {
+                            echo ' '. $value .'hr';
+                        } else {
+                            echo ' '. $value .'hrs';
+                        }
+                    } else if ($prepTimeNo == 1 && $value == 0) {
+                    }
+                    
+                    if ($prepTimeNo > 1 && $value >= 1) {
+                        if ($value == 1) {
+                            echo ' '. $value .'min';
+                        } else {
+                            echo ' '. $value .'mins';
+                        }
+                    } else if ($prepTimeNo > 1 && $value == 0) {
+                    }
+                }
+                echo '</p>';
+                
+                $cookArray = json_decode($row['cookTime']);
+                
+                $cookTimeNo = 0;
+                
+                echo '<p class="times" style="margin-right: 50px;"><b>Cooking time:</b>'; 
+                foreach ($cookArray as $value) { 
+                    $cookTimeNo++;
+                    if ($cookTimeNo == 1 && $value >= 1) {
+                        if ($value == 1) {
+                            echo ' '. $value .'hr';
+                        } else {
+                            echo ' '. $value .'hrs';
+                        }
+                    }
+                    
+                    if ($cookTimeNo > 1 && $value >= 1) {
+                        if ($value == 1) {
+                            echo ' '. $value .'min';
+                        } else {
+                            echo ' '. $value .'mins';
+                        }
+                    }
+                }
+                echo '</p>';
+                
+                echo '<p class="times"><b>Serves:</b> '. $row['serves'] . '</p>';
+                
+                echo '</div>';
+                echo '<div class="ingredientDis">';
+                echo '<h6>Ingredients</h6>';
+                
+                $ingredArray = json_decode($row['ingred']);
+                
+                foreach ($ingredArray as $value) { 
+                    echo '<p class="ingredList">' .$value. '</p>';
+                }
+                
+                echo '</div>';
+                echo '<br>';
+                
+                echo '<div class="instructionList">';
+                echo '<h6>Instructions</h6>';
+                
+                $instrucArray = json_decode($row['text']);
+                
+                echo '';
+                
+                foreach ($instrucArray as $value) { 
+                    $instrucNo++;
+                    echo '<p class="instructionList"><b class="instructionNo">' .$instrucNo. '. </b>' . $value . '<br></p>';
+                }
+                echo '';
+                
+                echo '</div>';
+                echo '</div>';
+            } else {
+                echo '<div id="contentPost" class="post-' . $row['id'] . '">';
+                echo '<div id="leftUserImg">';
+                    echo '<a href="profile.php?id=' . $row['userid'] . '">';
+                    if (!file_exists('img/avatars/' . $row['userid'] . '/avatar.jpg')) {
+                        echo '<img src="img/profile2.png" height="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    } else { 
+                        echo '<img src="img/avatars/' . $row['userid'] . '/avatar.jpg" height="50px" width="50px" style="border-radius:50%; border: 1px solid ' .$colour. '">';
+                    }
+                    echo '</a>';
+                echo '</div>';
+                echo $row['favourite'] ? '<div id="heart"><i class="fa fa-heart" style="cursor: default;"></i></div><div class="contentPostImage  imgNoPadding favouriteImg">' : '<div class="contentPostVideo imgNoPadding">';
+                echo '<div class="js-lazyYT" data-youtube-id="'.$row['text'].'" data-width="640px" data-height="361px"></div></div>'; 
+                
+            }
+                echo '<div id="contentInfoText">';
+                    echo '<div class="left"><a href="profile.php?id=' . $row['userid'] . '">' . $username . '</a></div>';
+                    echo '<div class="right">';
+
+                    if (($_SESSION['user']['rank'] != "user") && ($row['userid'] != $_SESSION['user']['id'])) {
+                        echo '<div class="timeago" style="padding-right: 17px;"><a href="post.php?id=' . $row['id'] . '">' . timeAgoInWords($row['date']) . '</a></div>';
+                        echo '<div class="admin post-' . $row['id'] . '"><i class="fa fa-trash-o"></i>';
+						echo ($row['favourite'] ? '<i class="fa fa-heart"></i>' : '<i class="fa fa-heart-o"></i>');
+						echo '</div>';
+                    } else {
+                        echo '<div class="timeago"><a href="post.php?id=' . $row['id'] . '">' . timeAgoInWords($row['date']) . '</a></div>';
+                    }
+                    echo '</div>';
+                echo '</div>';
+            echo '</div>';
+
+            if ($_SESSION['user']['id'] == $row['userid']) {
+                echo '<div id="contentLike" class="post-' . $row['id'] . '"><p class="delete">Delete</p>';
+                echo '<p class="totalToasts">' .$totalToasts. '</p></div>';
+            } else {
+                echo '<div id="contentLike" class="post-' . $row['id'] . '">';
+                if ($ifToasted == 0) {
+                    echo '<p class="toast">Toast</p>';
+                } else {
+                    echo '<p class="untoast">Toast</p>';
+                } 
+                if ($ifBurnt == 0) {
+                    echo '<p class="burn">Burn</p>';
+                } else {
+                    echo '<p class="unburn">Burn</p>';
+                }
+                echo '<p class="report">Report</p>';
+                echo '<p class="totalToasts">' .$totalToasts. '</div>'; 
+            }             
+            echo '</div>';
+        }
+    }
+        
+        
         
         <div id="content">
             <ul id="images">
