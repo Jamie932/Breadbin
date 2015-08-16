0efaa7fb4f0a3b4fad0971d78f249e712d7f4ce6
Marcus <BluntToothbrush>
2015-08-16 20:20:30 +0100

    Add soft border

--- php/recommendToaster.php
+++ php/recommendToaster.php
@@ -44,7 +44,7 @@
 				if (!file_exists('img/avatars/' . $row['id'] . '/avatar.jpg')) {
 					echo '<a href="profile.php?id=' . $user . '"><img src="img/profile2.png" height="54px" width="54px" style="border: 1px solid '.$colour.';"></a>';
 				} else { 
-					echo '<a href="profile.php?id=' . $user . '"><img src="img/avatars/' . $row['id'] . '/avatar.jpg" height="54px" width="54px"></a>';
+					echo '<a href="profile.php?id=' . $user . '"><img src="img/avatars/' . $row['id'] . '/avatar.jpg" height="54px" width="54px" style="border: 1px solid '.$colour.';"></a>';
 				}
 
 				echo '</div>';
