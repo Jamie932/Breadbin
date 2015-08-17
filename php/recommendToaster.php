a7d9ab965b1db04b006726a6f77021bf4a43a057
Jamie <Jamie932@gmail.com>
2015-08-17 22:21:29 +0100

    Undone changes

--- php/recommendToaster.php
+++ php/recommendToaster.php
@@ -27,7 +27,7 @@
 				echo '<a class="recomImg" href="profile.php?id=' . $user . '">
 					<img src="' . (file_exists('img/avatars/' . $row['id'] . '/avatar.jpg') ? "/img/avatars/" . $row['id'] . "/avatar.jpg" : "/img/profile2.png") . '" class="recomAvatarImg">
 
-					<span class="hoverSpan">
+					<span>
 						<div id="imageHoverLarge">
 							<img src="' . (file_exists('img/avatars/' . $row['id'] . '/avatar.jpg') ? "/img/avatars/" . $row['id'] . "/avatar.jpg" : "/img/profile2.png") . '"  width="165px" style="margin-top: -14px;">
 						</div>
