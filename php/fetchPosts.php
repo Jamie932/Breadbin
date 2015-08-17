f65a98749bc51ffbc1b1353ce67856ce3d85b5b0
Marcus <BluntToothbrush>
2015-08-17 22:41:27 +0100

    Remove hide

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -40,18 +40,15 @@
         if ($following == 0) {
                 echo '<div id="contentPost">';
                     echo '<div id="leftUserImg">';
-                        echo '<a href="profile.php?id=' . $userId . '">';
-                            echo file_exists($root . 'img/avatars/' . $userId . '/avatar.jpg') ? '<img src="/img/avatars/' . $userId . '/avatar.jpg" class="avatarImg">' : '<img src="/img/profile2.png" class="avatarImg">';
+                        echo '<a href="profile.php?id=' . $currentID . '">';
+                            echo file_exists($root . 'img/avatars/' . $currentID . '/avatar.jpg') ? '<img src="/img/avatars/' . $currentID . '/avatar.jpg" class="avatarImg">' : '<img src="/img/profile2.png" class="avatarImg">';
                         echo '</a>';
                     echo '</div>';
 
                     echo '<div class="contentPostText">';
                         echo '<p style="margin: 0;"><center>You don\'t follow any toasters.</center></p>';
                     echo '</div>';
-
-                    echo '<div id="contentLikeFollow">';
-                        echo '<p class="hide">Hide</p>';
-                    echo '</div>';  
+            
                 echo '</div>'; 
         }
         
