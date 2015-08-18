5d313659281f3aae5678c42a6b90a62a1140a7f4
Marcus <BluntToothbrush>
2015-08-18 15:58:11 +0100

    Link text posts

--- profile.php
+++ profile.php
@@ -221,7 +221,7 @@ if (empty($_GET)) {
                     echo '</li>'; 
             
                 } else if ($row['type'] == "text") {
-                        echo '<li><div class="box"><p class="textPost">' . $row['text'] . '</p></div></li>';
+                        echo '<a href="post.php?id=' . $row['id'] . '"><li><div class="box"><p class="textPost">' . $row['text'] . '</p></div></li></a>';
                     
                 } else if ($row['type'] == 'imagetext') {
                     $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $row['image']);
