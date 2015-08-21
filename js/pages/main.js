8d3b9949ae78108c740924439fe0163c548dcb78
Marcus <BluntToothbrush>
2015-08-21 11:34:11 +0100

    Try that

--- js/pages/main.js
+++ js/pages/main.js
@@ -273,11 +273,18 @@ $(document).ready(function(){
         $('.bodyHalfServe').on('keydown', '#recipeServe', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
     });
     
-    $("#contentAvatar").hoverIntent( makeTall, makeShort, '.hoverSpan' );
+    $(".hoverSpan").hoverIntent({
+        over: makeTall,
+        out: makeShort,
+        selector: '.recomImg'
+    });
     
 });
 
-function makeTall(){$(this).animate({"height":75},200);}
+function makeTall(){
+    $(this).animate({"height":75},200);
+    $(this).show;
+}
 function makeShort(){$(this).animate({"height":50},200);} 
 function show(){$(this).show;}
 
