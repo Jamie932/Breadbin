3a891e1462e8ec145e3bcd19d3f52146b2e79f9a
Marcus <BluntToothbrush>
2015-08-21 18:49:21 +0100

    Reduce speed

--- js/tileFunctions.js
+++ js/tileFunctions.js
@@ -32,14 +32,14 @@ $(document).ready(function() {
       boxes[i].style.position = 'relative';
     }
     
-    $("#main li").hoverIntent( makeTall, makeShort ); 
+    $("#main li").hoverIntent( showBottom, hideBottom ); 
 });
 
-function makeTall(){
-    $("#bottomImgTools").animate({"height":30},200);
-    $(".postUsername").animate({"bottom":"3px"},200);
+function showBottom(){
+    $("#bottomImgTools").animate({"height":30},100);
+    $(".postUsername").animate({"bottom":"3px"},100);
 }
-function makeShort(){
-    $("#bottomImgTools").animate({"height":0},200);
-    $(".postUsername").animate({"bottom":"-20px"},200);
+function hideBottom(){
+    $("#bottomImgTools").animate({"height":0},100);
+    $(".postUsername").animate({"bottom":"-20px"},100);
 } 
