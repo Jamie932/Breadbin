0dd5d4b7f0b1fcc2e2c58555c6daee0421741b6b
Jamie <Jamie932@gmail.com>
2015-08-23 03:12:55 +0100

    Trying something different again

--- js/pages/main.js
+++ js/pages/main.js
@@ -273,7 +273,7 @@ $(document).ready(function(){
         $('.bodyHalfServe').on('keydown', '#recipeServe', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
     });
     
-    $(".avHover").hoverIntent( function() { $(this).parent().find(".hoverSpan").stop(true, true).slideDown(); } , function() { $(this).parent().find(".hoverSpan").stop(true, true).slideUp(); }); 
+    $(".avHover").hoverIntent( function() { $(this).parent().find(".hoverSpan").stop().slideToggle(); } , function() { $(this).parent().find(".hoverSpan").stop().slideToggle(); }); 
 	
 });
 
