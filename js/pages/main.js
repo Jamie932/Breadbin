c3dead7157545deb8c742db682ed4338d3af7157
Jamie <Jamie932@gmail.com>
2015-08-23 02:22:42 +0100

    Trying to fix hoverIntent

--- js/pages/main.js
+++ js/pages/main.js
@@ -273,16 +273,9 @@ $(document).ready(function(){
         $('.bodyHalfServe').on('keydown', '#recipeServe', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
     });
     
-    $("#contentAvatar").hoverIntent( makeTall, makeShort ); 
+    $("#contentAvatar").hoverIntent( function() { $(this).find(".hoverSpan").slideDown(); } , function() { $(this).find(".hoverSpan").slideUp(); }); 
     
 });
-
-function makeTall(){
-    $(this).find(".hoverSpan").animate({"height":75},200);
-}
-function makeShort(){
-    $(this).find(".hoverSpan").animate({"height":0},200);
-} 
 
 $("#videoLink").keydown(function (e) {
      if (e.keyCode == 32) { 
