2db09cee52d03b5b346f88330d7fcedd76850c52
Marcus <BluntToothbrush>
2015-09-10 20:53:58 +0100

    Fix the favourite

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -168,7 +168,7 @@
 					</span>
 				</a>';
 					 echo '<div id="heart">';
-                        echo '$row['favourite'] ?<i class="fa fa-heart" style="cursor: default;"></i>' : '<i class="fa fa-heart" style="cursor: default; opacity: 0;"></i>';
+                        echo $row['favourite'] ? '<i class="fa fa-heart" style="cursor: default;"></i>' : '<i class="fa fa-heart" style="cursor: default; opacity: 0;"></i>';
 				    echo '</div>';
 				echo '</div>';
                     
