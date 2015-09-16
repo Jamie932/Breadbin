918ee15272c9ed9962a699c90fb26f33ef0821b1
Marcus <BluntToothbrush>
2015-09-16 15:28:51 +0100

    Swap around

--- php/fetchPosts.php
+++ php/fetchPosts.php
@@ -33,26 +33,7 @@
 
     $currentID = $_SESSION['user']['id'];
 
-    if (!$posts) {
-        echo '<div id="post">';
-				echo '<div id="contentAvatar">';
-					echo '<a href="profile.php?id=' . $currentID . '">';
-					echo file_exists($isRoot . 'img/avatars/' . $currentID . '/avatar.jpg') ? '<img src="/img/avatars/' . $currentID . '/avatar.jpg" class="avatarImg">' : '<img src="/img/defaultAvatar.png" class="avatarImg">';
-					echo '</a>';
-				echo '</div>';
-				echo '<div id="contentPost">';
-				   echo '<div class="contentPostText">';
-						echo '<p style="margin: 0;"><center>You or your toasters haven\'t posted anything!</p>';
-						echo '<p style="margin: 0;"><center><a id="showMePosts">Show me how to!</a></p>';
-                        echo '<div class="meter orange nostripes">
-                                <span style="width: 66%"></span>
-                            </div>
-                            </center>';
-					echo '</div>';
-				echo '</div>';
-			echo '</div>'; 
-		
-    } else if (!$posts && $following == 0) {
+    if (!$posts && $following == 0) {
         echo '<div id="post">';
 				echo '<div id="contentAvatar">';
 					echo '<a href="profile.php?id=' . $currentID . '">';
@@ -63,6 +44,25 @@
 				   echo '<div class="contentPostText">';
 						echo '<p style="margin: 0;"><center>You don\'t follow any toasters!</p>';
 						echo '<p style="margin: 0;"><center><a id="showMeToasters">Show me how to!</a></p>';
+                        echo '<div class="meter orange nostripes">
+                                <span style="width: 33%"></span>
+                            </div>
+                            </center>';
+					echo '</div>';
+				echo '</div>';
+			echo '</div>'; 
+        
+    } else if (!$posts) {
+        echo '<div id="post">';
+				echo '<div id="contentAvatar">';
+					echo '<a href="profile.php?id=' . $currentID . '">';
+					echo file_exists($isRoot . 'img/avatars/' . $currentID . '/avatar.jpg') ? '<img src="/img/avatars/' . $currentID . '/avatar.jpg" class="avatarImg">' : '<img src="/img/defaultAvatar.png" class="avatarImg">';
+					echo '</a>';
+				echo '</div>';
+				echo '<div id="contentPost">';
+				   echo '<div class="contentPostText">';
+						echo '<p style="margin: 0;"><center>You or your toasters haven\'t posted anything!</p>';
+						echo '<p style="margin: 0;"><center><a id="showMePosts">Show me how to!</a></p>';
                         echo '<div class="meter orange nostripes">
                                 <span style="width: 66%"></span>
                             </div>
